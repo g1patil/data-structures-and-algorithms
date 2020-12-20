65e0a88d6d5736434ac534fdf4bef8b38379d034
Jivan Patil <patil.jivan99@gmail.com>
2020-12-20 12:50:42 -0600

    sum of root to leaf

---
+++ src/trees/binary/PathSum.java
@@ -0,0 +1,47 @@
+package trees.binary;
+
+/*
+* Print the sum of all the possible paths from root to leaf
+* */
+public class PathSum {
+
+    public void calculateSum(Node node, int sum){
+
+        if (node.left == null && node.right == null){
+            System.out.println(sum + node.data);
+            return;
+        }
+
+        calculateSum(node.left,sum + node.data);
+        calculateSum(node.right,sum + node.data);
+
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(10);
+        Node n1 = new Node(5);
+        Node n2 = new Node(20);
+        Node n3 = new Node(4);
+        Node n4 = new Node(8);
+        Node n5 = new Node(14);
+        Node n6 = new Node(25);
+        Node n7 = new Node(6);
+        Node n8 = new Node(9);
+
+
+
+        root.left = n1;
+        root.right = n2;
+
+        n1.left = n3;
+        n1.right = n4;
+
+        n2.left = n5;
+        n2.right = n6;
+
+        n4.left = n7;
+        n4.right = n8;
+
+        new PathSum().calculateSum(root,0);
+    }
+}
