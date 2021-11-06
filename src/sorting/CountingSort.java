47830ced5379603db5807f99f08165f03998859c
jivan patil <jivan@tripaneer.com>
2021-11-06 21:14:55 +0100

    count sort version 2

--- src/sorting/CountingSort.java
+++ src/sorting/CountingSort.java
@@ -8,6 +8,10 @@ public class CountingSort {
 
     private int[] inputInts ;
 
+    /**
+     * Sorts or kind of sorts the elements using the count sort algorithm.
+     * This is simpler version.
+     * */
     private void countSort(){
         int MAX = Arrays.stream(this.inputInts).max().orElse(Integer.MIN_VALUE);
         int MIN = Arrays.stream(this.inputInts).min().orElse(0);
@@ -29,6 +33,27 @@ public class CountingSort {
             }
         }
     }
+
+    /**
+     *
+     * */
+    private void countSortVersion2(){
+        int MAX = Arrays.stream(this.inputInts).max().orElse(Integer.MIN_VALUE);
+        int MIN = Arrays.stream(this.inputInts).min().orElse(0);
+
+        int[] frequencyArray = new int[MAX -  MIN + 1 ];
+
+        for (int index = 0; index < inputInts.length; index++) {
+            frequencyArray[ inputInts[index] - MIN ] ++ ;
+        }
+
+        for (int index = 1 ; index < inputInts.length; index++) {
+            frequencyArray[  index  ] += frequencyArray[  index - 1  ] ;
+        }
+
+        int[] output = new int[this.inputInts.length];
+    }
+
     @Test
     public void test(){
         this.inputInts = new int[]{5,2,7,4,-2,2};
