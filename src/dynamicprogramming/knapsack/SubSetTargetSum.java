8987735f8b3b80c2efb66d1e102ce9701ca67ac5
Jivan <patil.jivan99@gmail.com>
2021-11-10 23:17:24 +0100

    target subset sum

--- src/dynamicprogramming/knapsack/SubSetTargetSum.java
+++ src/dynamicprogramming/knapsack/SubSetTargetSum.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 
 public class SubSetTargetSum {
 
+    boolean[][] memoization ;
+
     private boolean targetSubSetSum(int[] values , int pointer , int sum){
         if ( sum == 0)
             return true;
@@ -21,9 +23,46 @@ public class SubSetTargetSum {
         return targetSubSetSum( values , pointer , sum) ;
     }
 
+    /**
+     * Implement using the memoization.
+     * Improve this and change this to iterative
+     * */
+    private boolean targetSubSetSumMemoization(int[] values , int pointer , int sum){
+        int oldPointer = pointer ;
+        if ( sum == 0)
+            return true;
+        if ( (pointer == values.length && sum > 0) || pointer > memoization.length - 1 )
+            return false ;
+
+        if ( memoization[pointer][sum])
+            return true;
+
+        if ( values[pointer] <= sum){
+            int currentValue = values[pointer];
+            pointer++;
+            if (targetSubSetSumMemoization( values , pointer , sum - currentValue) ||
+                    targetSubSetSumMemoization( values , pointer , sum ) ){
+                return memoization[oldPointer][sum] = true ;
+            } ;
+        }
+        pointer++ ;
+        return memoization[oldPointer][sum] = targetSubSetSumMemoization( values , pointer , sum) ;
+    }
+
     @Test
     public void test(){
-        int[] values = new int[]{ 5,3,1,6,9,7 } ;
-        System.out.println( targetSubSetSum(values , 0 , 2 ));
+        int[] values = new int[]{ 5,3,1,6,4 } ;
+        int targetSum = 11 ;
+        System.out.println( targetSubSetSum(values , 0 , targetSum ));
+
+        memoization = new boolean[values.length + 1 ][targetSum + 1];
+
+        for (int i = 0; i < values.length + 1; i++) {
+            for (int j = 0; j < targetSum + 1; j++) {
+                memoization [i] [j] = false ;
+            }
+        }
+
+        System.out.println( targetSubSetSumMemoization(values , 0 , targetSum ));
     }
 }
