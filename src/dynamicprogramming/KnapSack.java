9a9ebaa002d91b0abdfa5636b6bc42271a6bc3dd
jivan patil <jivan@tripaneer.com>
2021-11-10 08:30:42 +0100

    dp bottom up approach

--- src/dynamicprogramming/KnapSack.java
+++ src/dynamicprogramming/KnapSack.java
@@ -11,7 +11,11 @@ import org.junit.Test;
 public class KnapSack {
 
     int[][] recursionResult ;
+    int[][] bottomUpResult ;
 
+    /**
+     * DP using the recursion
+     * */
     private int knapSackSolver(final int[] values , final int[] weight , int weightCapacity, int numberOfItems ){
         if ( weightCapacity == 0 || values.length == 0 || weight.length ==0 || numberOfItems == 0)
             return 0 ;
@@ -36,6 +40,9 @@ public class KnapSack {
         return knapSackSolver(values , weight , weightCapacity + 0 , numberOfItems -1 );
     }
 
+    /**
+     * DP using memoization
+     * */
     private int memoizationKnapSack(final int[] values , final int[] weight , int weightCapacity, int numberOfItems){
         if ( weightCapacity == 0 || values.length == 0 || weight.length ==0 || numberOfItems == 0)
             return 0 ;
@@ -62,6 +69,23 @@ public class KnapSack {
 
         return memoizationKnapSack(values , weight , weightCapacity + 0 , numberOfItems -1 );
     }
+
+
+    private int knapSackBottomUp(final int[] values , final int[] weight , int weightCapacity, int numberOfItems){
+        for (int i = 1; i < numberOfItems + 1; i++) {
+            for (int j = 1; j < weightCapacity + 1; j++) {
+                if ( weight[i -1 ] <= j){
+
+                     bottomUpResult[i][j] = Math.max(
+                             values[i -1 ] + bottomUpResult[i -1][j - weight[i -1 ] ]
+                                                        ,bottomUpResult[i -1 ][j ]
+                    );
+                } else bottomUpResult[i][j] = bottomUpResult[i - 1 ][j] ;
+            }
+        }
+
+        return bottomUpResult[numberOfItems][weightCapacity] ;
+    }
     @Test
     public void test(){
         int[] values = new int[]{1,2,10,3,4,6};
@@ -81,6 +105,16 @@ public class KnapSack {
             }
         }
 
+        this.bottomUpResult = new int[numberOfItems + 1 ][maxWeightCapacity + 1];
+
+        for (int i = 0; i < numberOfItems + 1 ; i++) {
+            for (int j = 0; j < maxWeightCapacity + 1; j++) {
+                if ( i == 0 || j == 0)
+                    bottomUpResult[i][j] = 0 ;
+            }
+        }
+
         System.out.println( this.memoizationKnapSack(values , weight , maxWeightCapacity , numberOfItems) );
+        System.out.println( this.knapSackBottomUp(values , weight , maxWeightCapacity , numberOfItems) );
     }
 }
