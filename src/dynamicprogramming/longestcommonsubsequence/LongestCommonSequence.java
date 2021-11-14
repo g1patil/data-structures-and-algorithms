170b4f04124ca9460e3cd7790f73bd0f0c47a94b
jivan patil <jivan@tripaneer.com>
2021-11-14 22:18:38 +0100

    changed the flags

--- src/dynamicprogramming/longestcommonsubsequence/LongestCommonSequence.java
+++ src/dynamicprogramming/longestcommonsubsequence/LongestCommonSequence.java
@@ -15,10 +15,10 @@ public class LongestCommonSequence {
      * */
     private int getLCSLength(String s1, String s2 , int p1 , int p2 ){
 
-        if ( p1 == 0 || p2 == 0)
+        if ( p1 < 0 || p2 < 0)
             return 0;
 
-        if ( s1.charAt(p1 -1 ) == s2.charAt(p2 -1 )){
+        if ( s1.charAt(p1) == s2.charAt(p2 )){
             return 1 + getLCSLength(s1,s2, p1 -1 ,p2 -1  );
         } else {
             return Math.max(getLCSLength(s1,s2, p1 -1 ,p2 ), getLCSLength(s1,s2,p1, p2 -1  ) ) ;
@@ -30,6 +30,6 @@ public class LongestCommonSequence {
     public void test(){
         String s1 = "ttjtttitttv";
         String s2 = "aaaaaaajivzzzzzz";
-        System.out.println(this.getLCSLength(s1,s2,s1.length() , s2.length() ));
+        System.out.println(this.getLCSLength(s1,s2,s1.length() -1 , s2.length() -1 ));
     }
 }
