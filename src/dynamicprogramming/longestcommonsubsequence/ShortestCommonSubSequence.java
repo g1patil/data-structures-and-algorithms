c982516671e39672d6fc3694ac79c2dd3539aca5
jivan patil <jivan@tripaneer.com>
2021-11-17 00:46:22 +0100

    short seqeunce

---
+++ src/dynamicprogramming/longestcommonsubsequence/ShortestCommonSubSequence.java
@@ -0,0 +1,23 @@
+package dynamicprogramming.longestcommonsubsequence;
+
+import org.junit.Test;
+
+public class ShortestCommonSubSequence {
+    /**
+     * TODO implement this using the recursion
+     * Not sure if its even possible
+     * */
+    private int getLCSLength(String s1, String s2 , int p1 , int p2 , int count ){
+
+    }
+
+    @Test
+    public void test2(){
+        String s1 = "aabbbcccccdddd";
+        String s2 = "qqqattttauuuuooooo";
+
+        System.out.println(this.getLCSLength(s1,s2,s1.length() , s2.length() , 0 ));
+
+
+    }
+}
