efa9a7c681fd8ddc3dee0b42efe65898dcae42b8
jivan patil <jivan@tripaneer.com>
2021-11-19 16:21:23 +0100

    mcm memoisation

--- src/dynamicprogramming/matrixchainmulti/MCM.java
+++ src/dynamicprogramming/matrixchainmulti/MCM.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 
 public class MCM {
 
+    int[][] memoization ;
+
     public int multiplyMatrix(int[] ints , int i , int j ){
         if ( i >= j)
             return 0 ;
@@ -18,10 +20,39 @@ public class MCM {
         return min ;
     }
 
+    public int multiplyMemoization(int[] ints , int i , int j ){
+
+        if ( i >= j)
+            return 0 ;
+
+        if (memoization[i][j] != -1 )
+            return memoization[i][j];
+
+        for (int k = i; k <= j -1 ; k++) {
+            memoization[i][j] = multiplyMemoization(ints , i , k ) +
+                    multiplyMemoization(ints , k+1 , j ) + ints[i-1]*ints[k]*ints[j];
+
+        }
+        return memoization[i][j] ;
+    }
+
     @Test
     public void test_(){
-        int[] ints = new int[]{1, 2, 3, 4};
+        int[] ints = new int[]{10, 30, 5, 60 };
         System.out.println(multiplyMatrix(ints , 1 , ints.length -1 ));
+    }
+
+    @Test
+    public void test_memoization(){
+        int[] ints = new int[]{10, 30, 5, 60 };
+        this.memoization = new int[ints.length][ints.length ];
+
+        for (int i = 0; i < ints.length; i++) {
+            for (int j = 0; j < ints.length; j++) {
+                    this.memoization[i][j] = -1 ;
+            }
+        }
+        System.out.println(multiplyMemoization(ints , 1 , ints.length -1 ));
     }
 
 }
