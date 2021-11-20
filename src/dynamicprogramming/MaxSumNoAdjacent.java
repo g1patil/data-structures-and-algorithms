17905f3de5bb3265f51318d843d400f018038f31
jivan patil <jivan@tripaneer.com>
2021-11-20 11:48:24 +0100

    max sum when no two elements are adjacent

---
+++ src/dynamicprogramming/MaxSumNoAdjacent.java
@@ -0,0 +1,36 @@
+package dynamicprogramming;
+
+import org.junit.Test;
+
+/**
+ * Maximum sum such that no two elements are adjacent
+ *
+ * Given an array of positive numbers, find the maximum sum of a subsequence with the constraint
+ * that no 2 numbers in the sequence should be adjacent in the array. So 3 2 7 10 should
+ * return 13 (sum of 3 and 10) or 3 2 5 10 7 should return 15 (sum of 3, 5 and 7).
+ * Answer the question in most efficient way.
+ *
+ * GFG : https://www.geeksforgeeks.org/maximum-sum-such-that-no-two-elements-are-adjacent/?ref=leftbar-rightbar
+ *
+ * */
+public class MaxSumNoAdjacent {
+
+    public int getMaxSum(int[] ints , int index , int max){
+
+        if (index > ints.length - 1)
+            return max ;
+
+        int temp = Math.max( getMaxSum( ints , index +  2 , max + ints[index]) ,
+                getMaxSum( ints , index +  1 , max  )  ) ;
+
+        max = Math.max( temp , max ) ;
+
+        return max ;
+
+    }
+    @Test
+    public void test_(){
+        int[] ints = new int[]{5,  5, 10, 40, 50, 35 };
+        System.out.println( getMaxSum( ints , 0 , 0 ));
+    }
+}
---
+++ src/dynamicprogramming/matrixchainmulti/EvaluateExpression.java
@@ -0,0 +1,32 @@
+package dynamicprogramming.matrixchainmulti;
+
+import org.junit.Test;
+
+/**
+ * Boolean Parenthesization Problem
+ *
+ * Given a boolean expression with following symbols.
+ *
+ * Symbols
+ *     'T' ---> true
+ *     'F' ---> false
+ *
+ * And following operators filled between symbols
+ *
+ * Operators
+ *     &   ---> boolean AND
+ *     |   ---> boolean OR
+ *     ^   ---> boolean XOR
+ * Count the number of ways we can parenthesize the expression so that the value of expression evaluates to true.
+ *
+ *
+ * */
+public class EvaluateExpression {
+
+    public int solve(String input , int i , int j ){
+        return - 1 ;
+    }
+    @Test
+    public void test_(){
+    }
+}
