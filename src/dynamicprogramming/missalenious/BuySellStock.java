7c6a7d112441bb404426f351c9026e19a9cc094f
jivan patil <jivan@tripaneer.com>
2021-11-21 21:19:38 +0100

    code clearn up

---
+++ src/dynamicprogramming/missalenious/BuySellStock.java
@@ -0,0 +1,20 @@
+package dynamicprogramming.missalenious;
+
+import org.junit.Test;
+/**
+ * Buy and sell the stock on the specific dats to maximize the profit at the end .
+ *
+ * @author g1patil 
+ * */
+public class BuySellStock {
+    private int getProfit(int[] stocks){
+         return -1 ;
+    }
+
+    @Test
+    public void test_(){
+        System.out.println(
+                getProfit(new int[]{10, 22, 5, 75, 65, 80})
+        );
+    }
+}
