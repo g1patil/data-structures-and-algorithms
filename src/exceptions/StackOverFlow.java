6f19b354f735450102cdbde810aff5f0468eb102
jivan patil <jivan@tripaneer.com>
2021-11-25 08:48:07 +0100

    added the comment

--- src/exceptions/StackOverFlow.java
+++ src/exceptions/StackOverFlow.java
@@ -5,6 +5,11 @@ Create a stackoverflow error
 public class StackOverFlow {
     
 
+    /**
+     * Call the method recursively to fill the stack.
+     * After number of calls, the stack should fill and
+     * method will throw stackoverflow exception.
+     * */
     void dummyMethod(){
         System.out.println("Calling the method");
         dummyMethod();
