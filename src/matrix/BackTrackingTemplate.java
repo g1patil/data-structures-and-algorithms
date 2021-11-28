e92bbd79bd4d4bb7b9fb9661de2e1b303fcee41d
jivan patil <jivan@tripaneer.com>
2021-11-28 22:45:42 +0100

    templte for backtracking

--- src/matrix/BackTrackingTemplate.java
+++ src/matrix/BackTrackingTemplate.java
@@ -1,8 +1,14 @@
 package matrix;
 
+import org.junit.Test;
+
 /**
  * @auther g1patil
  * This is the templte for Backtrcking algorithm
  */
 public class BackTrackingTemplate {
+
+    @Test
+    public void test_(){
+    }
 }
