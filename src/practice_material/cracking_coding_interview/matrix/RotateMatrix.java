7668769a623e317a7e3736c8b98a50b2df8d5955
jivan patil <jivan@tripaneer.com>
2021-12-04 13:22:49 +0100

    improving the matrix rotation

--- src/practice_material/cracking_coding_interview/matrix/RotateMatrix.java
+++ src/practice_material/cracking_coding_interview/matrix/RotateMatrix.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 public class RotateMatrix {
 
     int[][] matrix ;
+    boolean isClockwise ;
 
     private int getNewRow(int oldRow){
         return oldRow ;
@@ -18,37 +19,59 @@ public class RotateMatrix {
         return matrix.length - oldcolumn - 1 ;
     }
 
+    /**
+     * Returns the new coordinates based on the direction for the element in matrix.
+     * @param x old row index
+     * @param y old column index
+     * @return returns the int array of size 2.
+     * First element will be new row index and second will be new column index
+     * */
+    private int[] getNewCoordinates(int x , int y ){
+        if (isClockwise){
+            return new int[]{ y, matrix.length - x - 1 };
+        } else return new int[]{matrix.length - y - 1 , x};
+    }
+
     public void rotateMatrix(){
         if (matrix == null)
             return;
 
-        for (int i = 0; i < matrix[0].length -1 ; i++) {
-            int count = 4 ;
-            int currentRow = 0 , currentColumn = i ;
-            int newRow = 0 , newColumn = 0 ;
-            int oldTemp = matrix[currentRow][currentColumn] , newTemp = 0  ;
+        int diagonal = matrix.length/2;
 
-            while ( count > 0){
-                newRow = getNewRow(currentColumn);
-                newColumn = getNewColumn(currentRow);
-                newTemp = matrix[newRow][newColumn];
-                matrix[newRow][newColumn] = oldTemp ;
+        for (int diag = 1; diag <= diagonal; diag++) {
+            for (int i = diag -1 ; i < matrix[0].length - diag ; i++) {
+                int count = 4 ;
+                int currentRow = diag - 1  , currentColumn = i ;
+                int newRow = 0 , newColumn = 0 ;
+                int oldTemp = matrix[currentRow][currentColumn] , newTemp = 0  ;
 
-                oldTemp = newTemp ;
-                currentRow = newRow;
-                currentColumn = newColumn ;
-                count -- ;
+                while ( count > 0){
+                    newRow = getNewRow(currentColumn);
+                    newColumn = getNewColumn(currentRow);
+                    newTemp = matrix[newRow][newColumn];
+                    matrix[newRow][newColumn] = oldTemp ;
+
+                    oldTemp = newTemp ;
+                    currentRow = newRow;
+                    currentColumn = newColumn ;
+                    count -- ;
+                }
             }
         }
+
     }
     @Test
     public void test_(){
         this.matrix = new int[][]{
-                {1,2,3},
-                {4,5,6},
-                {7,8,9}
+                {1,2,3,4},
+                {5,6,7,8},
+                {9,10,11,12},
+                {13,14,15,16}
         };
 
+        MatrixUtility.print(matrix);
+        System.out.println("---------------------------");
+        rotateMatrix();
         rotateMatrix();
         MatrixUtility.print(matrix);
     }
