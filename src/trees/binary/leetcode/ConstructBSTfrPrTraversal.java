4478898610112ce9556a8520c14ebd0f372145f9
jivan patil <jivan@tripaneer.com>
2021-12-15 19:27:16 +0100

    tree from preorder arrray

--- src/trees/binary/leetcode/ConstructBSTfrPrTraversal.java
+++ src/trees/binary/leetcode/ConstructBSTfrPrTraversal.java
@@ -1,7 +1,35 @@
 package trees.binary.leetcode;
 
+import data.TreeNode;
+import org.junit.jupiter.api.Test;
+
 /**
  * @auther g1patil
  */
 public class ConstructBSTfrPrTraversal {
+
+    private TreeNode root;
+
+    private TreeNode addToTree( TreeNode root , int data){
+        if (root == null)
+            return new TreeNode(data);
+
+        if ( data < root.getData()){
+            root.left = addToTree(root.left , data);
+        } else root.right = addToTree(root.right , data);
+
+        return root;
+    }
+    public TreeNode bstFromPreorder(int[] preorder) {
+
+         for (int i : preorder)
+             root = addToTree(root , i);
+
+         return root;
+    }
+
+    @Test
+    public void test_(){
+        TreeNode.printTreeStatic( bstFromPreorder(new int[]{8,5,1,7,10,12}));
+    }
 }
