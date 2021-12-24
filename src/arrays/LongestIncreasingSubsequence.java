f20b6fedba11984dd3cad811483ac1ed4ed2101d
jivan patil <jivan@tripaneer.com>
2021-12-24 16:40:23 +0100

    memoization

--- src/arrays/LongestIncreasingSubsequence.java
+++ src/arrays/LongestIncreasingSubsequence.java
@@ -11,6 +11,8 @@ import org.junit.jupiter.api.Test;
 @Platform(Site.LEETCODE)
 public class LongestIncreasingSubsequence {
 
+    int[] memoization;
+
     //naive impl - time limit exceeded
     public int lengthOfLIS(int[] nums , int previousMax , int pointer , int sequneceLength) {
 
@@ -29,6 +31,30 @@ public class LongestIncreasingSubsequence {
         return lengthOfLIS(nums ,   previousMax , pointer + 1  , sequneceLength) ;
     }
 
+    public int lengthOfLISMemo(int[] nums) {
+
+        memoization = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            memoization[i] = 1 ;
+        }
+
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]){
+                    memoization[i] = Math.max( memoization[j] + 1 , memoization[i] );
+                }
+            }
+        }
+
+        int result = 1 ;
+
+        for (int i : memoization)
+            result = Math.max(result , i );
+
+        return result;
+    }
+
 
     public int lengthOfLIS(int[] nums) {
         return lengthOfLIS(nums , Integer.MIN_VALUE , 0  , 0 );
@@ -36,6 +62,11 @@ public class LongestIncreasingSubsequence {
 
     @Test
     public void test_(){
-        System.out.println(lengthOfLIS(new int[]{10 , 11 , 12 ,1,2 , 3 ,4 }));
+        System.out.println(lengthOfLISMemo(new int[]{10 , 11 , 12 ,1,2 , 3 ,4 }));
+    }
+
+    @Test
+    public void test_2(){
+        System.out.println(lengthOfLISMemo(new int[]{7,7,7,7,7,7,7}));
     }
 }
