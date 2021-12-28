6c61ede541a21dbd6a5777965b7d251dc1e953c0
jivan patil <jivan@tripaneer.com>
2021-12-28 17:56:50 +0100

    261. Graph Valid Tree - incomplete

--- src/graph/GraphValidTree.java
+++ src/graph/GraphValidTree.java
@@ -1,56 +1,21 @@
 package graph;
 
+import annotation.Platform;
+import annotation.Quality;
+import annotation.Site;
+import annotation.Stage;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
-import java.util.Queue;
+import java.util.*;
 
+@Quality(value = Stage.INCOMPLETE , details = "implement using union find")
+@Platform(Site.LEETCODE)
 public class GraphValidTree {
 
-    Map<Integer , Queue<Integer>> graph = new HashMap<>();
-    Map<Integer , Integer> degreeMap = new HashMap<>();
 
     public boolean validTree(int n, int[][] edges) {
-
-        buildGraph(edges);
         return false;
     }
 
-    private void buildGraph(int[][] edges) {
 
-        for (int[] pair : edges){
-            int source = pair[0];
-            int destination = pair[1];
-
-            Queue sourceQueue = graph.get(source);
-            Queue destQueue = graph.get(source);
-
-            if (sourceQueue == null){
-                sourceQueue = new LinkedList();
-            }
-
-            if (destQueue == null){
-                destQueue = new LinkedList();
-            }
-
-            sourceQueue.add(destination);
-            destQueue.add(source);
-
-
-        }
-    }
-
-    [],[1,2],[2,3],[1,3],[1,4]
-    @Test
-    public void test(){
-        int[][] matrix = new int[][]{
-                new int[]{},
-                new int[]{},
-                new int[]{},
-                new int[]{},
-                new int[]{},
-        }
-    }
 }
