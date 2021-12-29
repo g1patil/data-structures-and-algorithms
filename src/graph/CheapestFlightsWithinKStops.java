6b9412b2088dc595094b31b89228f218a6da649b
jivan patil <jivan@tripaneer.com>
2021-12-29 12:26:50 +0100

    787. Cheapest Flights Within K Stops - improved

--- src/graph/CheapestFlightsWithinKStops.java
+++ src/graph/CheapestFlightsWithinKStops.java
@@ -63,6 +63,51 @@ public class CheapestFlightsWithinKStops {
         return ;
     }
 
+
+
+
+    /**
+     * Smarter way to implement the Dijkstra from LC comment section
+     * https://leetcode.com/problems/cheapest-flights-within-k-stops/discuss/1624971/Java-Solution
+     * */
+    public int findCheapestPriceFaster(int n, int[][] flights, int src, int dst, int k) {
+        Map<Integer, List<int[]>> graph = new HashMap<>();
+        Map<Integer, Integer> shortestPathsToNodes = new HashMap<>();
+        prepareGraph(flights, graph);
+        int minDist = Integer.MAX_VALUE;
+
+        Queue<int[]> queue =  new LinkedList();
+        queue.add(new int[]{src , 0 , k});
+
+        while(!queue.isEmpty()){
+            int[] current = queue.poll();
+
+            if (current[0] == dst){
+                minDist = Math.min(minDist , current[1]);
+                continue;
+            }
+
+            if (!graph.containsKey(current[0]) || current[2] < 0)
+                continue;
+
+            for (int[] next : graph.get(current[0])){
+                if (!shortestPathsToNodes.containsKey(next[0]) || current[1] + next[1] < shortestPathsToNodes.get(next[0])){
+                    shortestPathsToNodes.put(next[0], current[1] + next[1] );
+                    queue.add(new int[]{next[0] , current[1] + next[1] , current[2] -1});
+                }
+            }
+        }
+
+        return minDist == Integer.MAX_VALUE ? -1 : minDist;
+    }
+
+    public void prepareGraph(int[][] flights, Map<Integer, List<int[]>> map){
+        for(int[] trip : flights){
+            map.putIfAbsent(trip[0], new ArrayList<>());
+            map.get(trip[0]).add(new int[]{trip[1], trip[2]});
+        }
+    }
+
     @Test
     public void test_(){
         int[] e1 = new int[]{0,1,100};
@@ -72,6 +117,20 @@ public class CheapestFlightsWithinKStops {
         int[][] g = new int[][]{e1,e2,e3};
 
         System.out.println(findCheapestPrice(3 , g , 0  , 2 , 1));
+    }
+
+    @Test
+    public void test_3(){
+        int[] e1 = new int[]{0,1,1};
+        int[] e2 = new int[]{0,2,2};
+        int[] e3 = new int[]{0,3,0};
+        int[] e4 = new int[]{1,4,2};
+        int[] e5 = new int[]{2,4,1};
+        int[] e6 = new int[]{3,4,1};
+
+        int[][] g = new int[][]{e1,e2,e3,e4,e5,e6};
+
+        System.out.println(findCheapestPrice(3 , g , 0  , 4 , 2));
     }
 
     @Test
@@ -85,6 +144,6 @@ public class CheapestFlightsWithinKStops {
 
         int[][] g = new int[][]{e1,e2,e3 ,e4,e5,e6};
 
-        System.out.println(findCheapestPrice(5 , g , 2  , 1 , 1));
+        System.out.println(findCheapestPriceFaster(5 , g , 0  , 2 , 3));
     }
 }
