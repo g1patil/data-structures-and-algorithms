5f80f33c82a428624ecf7e9b9f6b5bd27a2263d9
jivan patil <jivan@tripaneer.com>
2022-01-01 18:15:25 +0100

    1. Two Sum = final

--- src/arrays/TwoSum.java
+++ src/arrays/TwoSum.java
@@ -1,9 +1,16 @@
 package arrays;
 
+import annotation.Platform;
+import annotation.Quality;
+import annotation.Site;
+import annotation.Stage;
+
 import java.util.HashMap;
 import java.util.Map;
 
 /***
+ * 1. Two Sum
+ *
  * Given an array of integers nums and an integer target, return indices of the
  * two numbers such that they add up to target. You may assume that each input
  * would have exactly one solution, and you may not use the same element twice.
@@ -18,6 +25,8 @@ import java.util.Map;
  * 
  * 
  **/
+@Quality(Stage.TESTED)
+@Platform(Site.LEETCODE)
 public class TwoSum {
 
     void getTargetElements(int[] input,int targetSum){
@@ -36,6 +45,29 @@ public class TwoSum {
             }
         }
     }
+
+    /**
+     * LC submission
+     * @param nums int numbers
+     * @param target target sum that we want to find pair of
+     * */
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        Map<Integer , Integer> map = new HashMap();
+        for(int i = 0 ; i < nums.length ; i ++){
+            int current = nums[i];
+
+            if(map.containsKey(target - current)){
+                result[0] = i ;
+                result[1] = map.get(target-current);
+                return result;
+            } else map.put(current , i);
+        }
+
+        return result;
+
+    }
+
     public static void main(String[] args) {
         new TwoSum().getTargetElements(new int[]{1,3,5,8,11}, 12);
     }
