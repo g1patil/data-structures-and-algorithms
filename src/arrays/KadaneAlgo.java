7d73204f982eb6699604fd25c720700e5d73ec44
jivan patil <jivan@tripaneer.com>
2022-01-03 23:31:48 +0100

    53 . Maximum Subarray : WIP

---
+++ src/arrays/KadaneAlgo.java
@@ -0,0 +1,14 @@
+package arrays;
+
+import annotation.Platform;
+import annotation.Quality;
+import annotation.Site;
+
+/**
+ * @author g1patil
+ * 53 . Maximum Subarray
+ *
+ */
+@Platform(Site.LEETCODE)
+public class KadaneAlgo {
+}
