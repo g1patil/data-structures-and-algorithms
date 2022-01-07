5285393979e1538df9d6ee7958f064b55348fd79
jivan patil <jivan@tripaneer.com>
2022-01-07 18:49:54 +0100

    560. Subarray Sum Equals K 89/90 passes - final

---
+++ src/arrays/SubarraySumEqualsK.java
@@ -0,0 +1,35 @@
+package arrays;
+
+import annotation.Platform;
+import annotation.Site;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author g1patil
+ * 560. Subarray Sum Equals K
+ */
+@Platform(Site.LEETCODE)
+public class SubarraySumEqualsK {
+
+    public int subarraySum(int[] nums, int k) {
+        int count = 0 ;
+
+        for (int i = 0; i < nums.length; i++) {
+            int sum = 0 ;
+            for (int j = i; j < nums.length; j++) {
+                sum = sum + nums[j];
+                if (sum == k){
+                    count ++;
+                    continue;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    @Test
+    public void test_(){
+        System.out.println(subarraySum(new int[]{1,1,1} , 2 ));
+    }
+}
