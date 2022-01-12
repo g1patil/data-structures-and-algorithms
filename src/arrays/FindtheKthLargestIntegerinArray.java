d0a73cfbe10892939817601b7b07704346eba9d3
jivan patil <jivan@tripaneer.com>
2022-01-12 19:43:12 +0100

    1985. Find the Kth Largest Integer in the Array - final

---
+++ src/arrays/FindtheKthLargestIntegerinArray.java
@@ -0,0 +1,43 @@
+package arrays;
+
+import annotation.Platform;
+import annotation.Quality;
+import annotation.Site;
+import annotation.Stage;
+import org.junit.jupiter.api.Test;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * @author g1patil
+ * 1985. Find the Kth Largest Integer in the Array
+ */
+@Quality(Stage.TESTED)
+@Platform(Site.LEETCODE)
+public class FindtheKthLargestIntegerinArray {
+
+    public String kthLargestNumber(String[] nums, int k) {
+        PriorityQueue<String> queue= new PriorityQueue<String>((o1,o2)->{
+            if (o1.length() == o2.length()){
+                return o1.compareTo(o2);
+            }
+
+            return Integer.compare(o1.length(),o2.length());
+        });
+
+        for (String s : nums){
+            queue.add(s);
+        }
+
+
+        while (queue.size() > k)
+            queue.poll();
+        return queue.peek();
+    }
+
+    @Test
+    public void test_(){
+        System.out.println(kthLargestNumber(new String[]{"39","31","36","760","100"}, 4 ));
+    }
+}
