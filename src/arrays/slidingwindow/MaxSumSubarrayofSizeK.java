48e4935248cbfff87da0db2b603c37b8989d38f4
jivan patil <jivan@tripaneer.com>
2022-01-16 08:36:38 +0100

    Max Sum Subarray of size K - GFG - Final

---
+++ src/arrays/slidingwindow/MaxSumSubarrayofSizeK.java
@@ -0,0 +1,35 @@
+package arrays.slidingwindow;
+
+import annotation.Platform;
+import annotation.Quality;
+import annotation.Site;
+import annotation.Stage;
+
+import java.util.ArrayList;
+
+/**
+ * @author g1patil
+ * Given an array of integers Arr of size N and a number K. Return the maximum sum of a subarray of size K.
+ */
+@Quality(Stage.TESTED)
+@Platform(Site.GEEKFORGEEKS)
+public class MaxSumSubarrayofSizeK {
+
+    static int maximumSumSubarray(int K, ArrayList<Integer> Arr, int N){
+        int start = 0 , end = 0 , sum = 0 , max = Integer.MIN_VALUE;
+
+        while (end < Arr.size()){
+            sum+=Arr.get(end);
+            if ( end - start + 1 < K ){
+                end ++;
+            } else if( end - start + 1 == K){
+                max = Math.max( max , sum);
+                sum-=Arr.get(start);
+                start++;
+                end++;
+            }
+        }
+
+        return max;
+    }
+}
