d3ea4db9ab88785c819c8dcfc9d44649f3f9f77d
jivan patil <jivan@tripaneer.com>
2022-01-16 18:27:15 +0100

    560. Subarray Sum Equals K - final - reference leetcode discussion section

--- src/arrays/SubarraySumEqualsK.java
+++ src/arrays/SubarraySumEqualsK.java
@@ -4,6 +4,9 @@ import annotation.Platform;
 import annotation.Site;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author g1patil
  * 560. Subarray Sum Equals K
@@ -13,24 +16,23 @@ public class SubarraySumEqualsK {
 
     // TLE time limit exceeds - not recommended
     public int subarraySum(int[] nums, int k) {
-        int count = 0 ;
+        int sum = 0, result = 0;
+        Map<Integer, Integer> preSum = new HashMap<>();
+        preSum.put(0, 1);
 
         for (int i = 0; i < nums.length; i++) {
-            int sum = 0 ;
-            for (int j = i; j < nums.length; j++) {
-                sum = sum + nums[j];
-                if (sum == k){
-                    count ++;
-                    continue;
-                }
+            sum += nums[i];
+            if (preSum.containsKey(sum - k)) {
+                result += preSum.get(sum - k);
             }
+            preSum.put(sum, preSum.getOrDefault(sum, 0) + 1);
         }
 
-        return count;
+        return result;
     }
 
     @Test
     public void test_(){
-        System.out.println(subarraySum(new int[]{1,1,1} , 2 ));
+        System.out.println(subarraySum(new int[]{3,4,7,2,-3,1,4,2} , 7 ));
     }
 }
