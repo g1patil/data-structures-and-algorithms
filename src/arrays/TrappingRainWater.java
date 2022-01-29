6f6ffa185074f4156f8f5ba8e2f896578cf4da88
jivan patil <jivan@tripaneer.com>
2022-01-29 14:40:42 +0100

    42. Trapping Rain Water - final

--- src/arrays/TrappingRainWater.java
+++ src/arrays/TrappingRainWater.java
@@ -8,8 +8,27 @@ import org.junit.jupiter.api.Test;
 
 /**
  * @author g1patil
+ *
+ * 42. Trapping Rain Water
+ * Algo :
+ * look at optimal solution.
+ * Two pointer. left and right
+ * create leftmax and rightMax
+ *
+ * while (left < right){
+ *     if left element is < right element
+ *       then bring everyone to your level
+ *       if found bigger than you , then
+ *          make that guy leftmax
+ *       else add the height difference to water
+ *
+ *     else
+ *       then bring everyone to your level
+ *       if found bigger than you , then
+ *          make that guy leftmax
+ *       else add the height difference to water
+ * }
  */
-
 @Quality(Stage.DOCUMENTED)
 @Platform(Site.LEETCODE)
 public class TrappingRainWater {
@@ -57,6 +76,41 @@ public class TrappingRainWater {
         for (int i : height)
             max = Math.max(i , max );
         return max;
+    }
+
+    /**
+     * Main solution.
+     * @author https://leetcode.com/AndresValdes
+     * */
+    public int trapOptimal(int[] height){
+        if(height == null || height.length == 0)
+            return 0;
+
+        int left = 0 , right = height.length -1 ;
+        int leftMax = 0 , rightMax = 0 ;
+
+        int water = 0 ;
+
+        while(left < right){
+            if(height[left] < height[right]){
+
+                if(height[left] >= leftMax){
+                    leftMax = height[left];
+                } else {
+                    water+= leftMax - height[left];
+                }
+                left++;
+
+            } else {
+                if(height[right] >= rightMax){
+                    rightMax = height[right];
+                } else {
+                    water+= rightMax - height[right];
+                }
+                right--;
+            }
+        }
+        return water;
     }
 
     @Test
