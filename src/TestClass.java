6169022a00fae7c077bb6310da2267311ea5d57f
jivan patil <jivan@tripaneer.com>
2022-01-29 16:45:40 +0100

    339. Nested List Weight Sum - optimal  - final

---
+++ src/TestClass.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test class to practice any given problem .
+ * This class serves as the template class for any coding practices that I do.
+ * */
+public class TestClass {
+
+    public void test(int n){
+        while (true){
+            System.out.println(n);
+            n++;
+        }
+    }
+    @Test
+    public void test_(){
+        test(1);
+    }
+}
--- src/arrays/NestedListWeightSum.java
+++ src/arrays/NestedListWeightSum.java
@@ -55,5 +55,35 @@ class Solution {
         depthSumHelper(nestedList , 1);
         return sum;
     }
+
+    Optimal solution :
+
+    public int depthSum(List<NestedInteger> nestedList) {
+    if(nestedList == null){
+        return 0;
+    }
+
+    int sum = 0;
+    int level = 1;
+
+    Queue<NestedInteger> queue = new LinkedList<NestedInteger>(nestedList);
+    while(queue.size() > 0){
+        int size = queue.size();
+
+        for(int i = 0; i < size; i++){
+            NestedInteger ni = queue.poll();
+
+            if(ni.isInteger()){
+                sum += ni.getInteger() * level;
+            }else{
+                queue.addAll(ni.getList());
+            }
+        }
+
+        level++;
+    }
+
+    return sum;
+    }
 }
 */
