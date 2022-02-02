90bac876a4e93d7bb6eb760bdeea313af17794f7
jivan patil <jivan@tripaneer.com>
2022-02-02 22:46:05 +0100

    ransom note practice - practice

--- src/TestClass.java
+++ src/TestClass.java
@@ -9,68 +9,31 @@ import java.util.*;
 public class TestClass {
 
 
+    public boolean canConstruct(String ransomNote, String magazine) {
+        if(ransomNote == null)
+            return true;
+        Map<Character,Integer> map = new HashMap();
 
+        for(char c : magazine.toCharArray()){
+            if(map.containsKey(c)){
+                map.put(c , map.get(c) + 1 );
+            } else map.put(c , 1);
+        }
 
-    public double[] medianSlidingWindow(int[] nums, int k) {
-
-        PriorityQueue<Integer> min = new PriorityQueue();
-        PriorityQueue<Integer> max = new PriorityQueue(Comparator.reverseOrder());
-
-        double[] result = new double[nums.length  - k + 1];
-
-
-        for(int i = 0 ; i < nums.length ; i ++){
-
-            if(max.isEmpty() || max.peek() > nums[i]){
-                max.add(nums[i]);
+        for(char c : ransomNote.toCharArray()){
+            if(!map.containsKey(c) || map.get(c) == 0){
+                return false;
             } else {
-                min.add(nums[i]);
+                map.put(c , map.get(c) -1 );
             }
-
-            balanceHeap(max, min);
-
-            if(min.size() + max.size() == 3){
-                double med = 0 ;
-
-                if(max.size() > min.size()){
-                    med = max.peek();
-                } else if (min.size() > max.size()){
-                    med = min.peek();
-                } else {
-                    med = (double) (max.peek() + min.peek())/2;
-                }
-
-                int index = i - k + 1;
-                result[index] = med;
-
-                if(!max.remove(nums[index])){
-                    min.remove(nums[index]);
-                }
-                index++;
-                balanceHeap(max,min);
-
-            }
-
         }
 
-        return result;
-    }
-
-    public void balanceHeap(PriorityQueue<Integer> first , PriorityQueue<Integer> second){
-        if(first.size() > second.size() + 1){
-            second.add(first.poll());
-        } else if (second.size() > first.size() + 1){
-            first.add(second.poll());
-        }
+        return true;
     }
 
     @Test
     public void test_(){
-        System.out.println(medianSlidingWindow(new int[]{1,3,-1,-3,5,3,6,7} , 3 ));
+        System.out.println(canConstruct("hello" , "hlo world"));
     }
 
-    @Test
-    public void test_2(){
-        System.out.println(medianSlidingWindow(new int[]{1,4,2,3} , 3 ));
-    }
 }
