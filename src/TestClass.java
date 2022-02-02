491fc5c693830d8ebf5c800d386173eb2da8eb23
jivan patil <jivan@tripaneer.com>
2022-02-02 23:41:17 +0100

    609. Find Duplicate File in System - practice

--- src/TestClass.java
+++ src/TestClass.java
@@ -10,18 +10,40 @@ import java.util.*;
 public class TestClass {
 
 
-    public int maxDepth(TreeNode root) {
-        return depthDFS(root);
+
+    public List<List<String>> findDuplicate(String[] paths) {
+        Map<String,List<String>> map = new HashMap();
+        List<List<String>> result = new ArrayList();
+        for(String path : paths){
+
+            String[] p = path.split(" ");
+
+
+            for(int i  = 1 ; i < p.length ; i ++){
+                String dir = p[0];
+                String content = p[i].substring( p[i].indexOf('(') + 1 , p[i].indexOf(')') );
+
+                if(!map.containsKey(content)){
+                    List<String> list = new ArrayList();
+                    list.add( dir + "/" + p[i].substring(0, p[i].indexOf('(')));
+                    map.put(content , list);
+                } else {
+                    map.get(content).add(dir + "/" + p[i].substring(0, p[i].indexOf('(')));
+                }
+            }
+
+        }
+
+        map.keySet().stream().filter( k-> map.get(k).size() > 1).forEach(
+                k-> result.add(map.get(k)));
+
+        return result;
     }
 
-    public int depthDFS(TreeNode root){
-        if(root == null)
-            return 0;
-
-        int left = depthDFS(root.left);
-        int right = depthDFS(root.right);
-
-        return 1 + Math.max(left , right);
+    @Test
+    public void test_(){
+        String[] path = new String[]{"root/a 1.txt(abcd) 2.txt(efgh)","root/c 3.txt(abcd)","root/c/d 4.txt(efgh)","root 4.txt(efgh)"};
+        System.out.println(findDuplicate(path));
     }
 
 }
