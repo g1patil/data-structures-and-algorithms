864cce4240fb9d089616aa2602315aa5c8622ee4
jivan patil <jivan@tripaneer.com>
2022-02-12 23:59:46 +0100

    103. Binary Tree Zigzag Level Order Traversal

--- src/trees/binary/BinaryTreeZigzagLevelOrderTraversal.java
+++ src/trees/binary/BinaryTreeZigzagLevelOrderTraversal.java
@@ -14,7 +14,7 @@ import java.util.Queue;
 
 /**
  * @author g1patil
- * 103. Binary Tree Zigzag Level Order
+ * 103. Binary Tree Zigzag Level Order Traversal
  */
 @Platform(Site.LEETCODE)
 public class BinaryTreeZigzagLevelOrderTraversal {
