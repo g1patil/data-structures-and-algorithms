a48f529a7053916ce43e90301775c0c7498fb8eb
jivan patil <jivan@tripaneer.com>
2022-02-18 23:43:38 +0100

    pick random index

--- src/TestClass.java
+++ src/TestClass.java
@@ -11,102 +11,57 @@ import java.util.stream.IntStream;
 public class TestClass {
 
 
-    public int maxEnvelopes(int[][] envelopes) {
-        Arrays.sort(envelopes, new Comparator<int[]>() {
-            public int compare(int[] arr1, int[] arr2) {
-                if (arr1[0] == arr2[0]) {
-                    return arr2[1] - arr1[1];
-                } else {
-                    return arr1[0] - arr2[0];
-                }
-            }
-        });
-        int[] nums = new int[envelopes.length * 2];
-
-        int i = 0 ;
-        for(int[] num : envelopes){
-            Arrays.sort(num);
-            nums[i++] = num[0];
-            nums[i++] = num[1];
+    ArrayList<Long> prob = new ArrayList();
+    Map<Long, Integer> map = new HashMap();
+    public TestClass(int[] w) {
+        int sum = 0;
+        for(int i : w){
+            sum+=i;
         }
 
-        return lengthOfLIS(nums);
+        Long previous = 0L ;
+        for(int i = 0 ; i < w.length ;i++){
+            Long p = Math.round(((double)w[i]/sum)*100);
+            map.put(p + previous , i);
+            prob.add(p+ previous);
+            previous+=p;
+        }
+        // System.out.print(prob);
+        System.out.print(map);
+
+        Collections.sort(prob);
     }
 
-    public int lengthOfLIS(int[] nums) {
-        int[] memoization = memoization = new int[nums.length];
+    public int getIndex(long p){
+        int low= 0 , high = prob.size();
 
-        for (int i = 0; i < nums.length; i++) {
-            memoization[i] = 1 ;
-        }
+        while(low < high){
+            int mid = low + (high - low)/2 ;
 
-        for (int i = 1; i < nums.length; i++) {
-            for (int j = 0; j < i; j++) {
-                if (nums[j] < nums[i]){
-                    memoization[i] = Math.max( memoization[j] + 1 , memoization[i] );
-                }
+            if(prob.get(mid) < p){
+                low = mid + 1;
+            } else{
+                high = mid;
             }
         }
 
-        int result = 1 ;
-
-        for (int i : memoization)
-            result = Math.max(result , i );
-
-        return result;
+        return high;
     }
 
-    @Test
-    public void test_(){
-
-       int[][] ints = new int[][]{
-               new int[]{5,4},
-               new int[]{6,4},
-               new int[]{6,7},
-               new int[]{2,3}
-       };
-
-       maxEnvelopes(ints);
+    public int pickIndex() {
+        int index = getIndex( Math.round(Math.random()*100));
+        return map.get(prob.get(index == prob.size() ? index -1 : index));
     }
 
-    @Test
-    public void test_2(){
-
-      divide(-1,-1);
-    }
-
-    public int divide(int dividend, int divisor) {
-        int sign  = (divisor < 0 && dividend < 0) ? 1 : (divisor < 0 || dividend < 0) ? -1 : 1 ;
-        int result = 0 ;
-        divisor = Math.abs(divisor);
-        dividend = Math.abs(dividend);
-
-        while(dividend >= divisor){
-
-            dividend-=divisor;
-            result++;
-            System.out.println(result);
-        }
-        return result*sign;
-    }
-
-    @Test
-    public void test_3(){
-        List<SomeClass> list = new ArrayList<>();
-        list.add(new SomeClass(1,"jivan"));
-        list.add(new SomeClass(2,"manoj"));
-        list.add(new SomeClass(3,"dhana"));
-
-
-        int i = 0;
-        IntStream.rangeClosed(1, Integer.MAX_VALUE).asLongStream().forEach(System.out::println);
-    }
-
-    class SomeClass {
-        int id;
-        String name;
-        SomeClass(int i , String name){this.id = i ; this.name = "jivan";}
-
+    public static void main(String[] args) {
+        TestClass testClass = new TestClass(new int[]{3,14,1,7});
+        System.out.println(testClass.pickIndex());
+        System.out.println(testClass.pickIndex());
+        System.out.println(testClass.pickIndex());
+        System.out.println(testClass.pickIndex());
+        System.out.println(testClass.pickIndex());
+        System.out.println(testClass.pickIndex());
+        System.out.println(testClass.pickIndex());
     }
 
 
