65a9a5e869004ea6c18f9ee7bbb3f08bbae374ab
jivan patil <jivan@tripaneer.com>
2022-02-20 20:11:17 +0100

    155. Min Stack - final

---
+++ src/trees/MinStack.java
@@ -0,0 +1,44 @@
+package trees;
+
+import annotation.Platform;
+import annotation.Quality;
+import annotation.Site;
+import annotation.Stage;
+
+import java.util.Stack;
+
+/**
+ * @author g1patil
+ * 155. Min Stack
+ */
+@Quality(Stage.TESTED)
+@Platform(Site.LEETCODE)
+public class MinStack {
+
+    private Stack<int[]> stack = new Stack();
+    int min = Integer.MAX_VALUE;
+
+    public MinStack() {
+    }
+
+    public void push(int val) {
+        if(stack.isEmpty()){
+            stack.push(new int[]{val , val});
+            return;
+        } else{
+            stack.push(new int[]{val , Math.min(val,stack.peek()[1])});
+        }
+    }
+
+    public void pop() {
+        stack.pop();
+    }
+
+    public int top() {
+        return  stack.peek()[0];
+    }
+
+    public int getMin() {
+        return stack.peek()[1];
+    }
+}
