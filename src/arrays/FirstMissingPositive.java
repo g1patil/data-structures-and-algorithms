b9387b4480322a2b5da3afe8aebbdb8f598235e5
jivan patil <jivan@tripaneer.com>
2022-02-21 16:46:50 +0100

    41. First Missing Positive - final

--- src/arrays/FirstMissingPositive.java
+++ src/arrays/FirstMissingPositive.java
@@ -14,28 +14,20 @@ import annotation.Stage;
 public class FirstMissingPositive {
 
     public int firstMissingPositive(int[] nums) {
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
+        int[] missing = new int[nums.length + 1];
 
-        for(int i : nums){
-            if(i<=0)
-                continue;
-            min = Math.min(min , i);
-            max = Math.max(max , i);
+        for(int val : nums){
+            if(val < missing.length && val >=0 )
+                missing[val] = - 1;
         }
 
-        if(min!=1)
-            return 1;
-        int sum = 0 ;
-        for(int i = min ; i <=max ; i++)
-            sum+=i;
+        int i = 1;
 
-        for(int i : nums){
-            if(i<=0)
-                continue;
-            sum-=i;
+        for( ; i < missing.length ; i++){
+            if(missing[i]!= -1)
+                return i;
         }
 
-        return sum== 0 ? max+1 : sum;
+        return i;
     }
 }
