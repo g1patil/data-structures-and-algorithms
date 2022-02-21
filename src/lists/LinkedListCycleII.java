7f52fa09be0bf0679958d12891b7eb87a3f311cb
jivan patil <jivan@tripaneer.com>
2022-02-21 10:32:47 +0100

    142. Linked List Cycle II- final

---
+++ src/lists/LinkedListCycleII.java
@@ -0,0 +1,44 @@
+package lists;
+
+import annotation.Platform;
+import annotation.Quality;
+import annotation.Site;
+import annotation.Stage;
+
+/**
+ * @author g1patil
+ * 142. Linked List Cycle II
+ */
+@Quality(Stage.TESTED)
+@Platform(Site.LEETCODE)
+public class LinkedListCycleII {
+
+    public ListNode detectCycle(ListNode head) {
+        if (head == null)
+            return head;
+
+        ListNode collNode = getCollisionNode(head);
+        ListNode tempHead = head ;
+
+        while (collNode !=tempHead){
+            collNode = collNode.next;
+            tempHead = tempHead.next;
+        }
+
+        return collNode;
+    }
+
+    private ListNode getCollisionNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast!=null && fast.next !=null){
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast)
+                return slow;
+        }
+        return null;
+    }
+}
