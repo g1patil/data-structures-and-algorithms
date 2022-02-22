be4305f2d36edfef913e08307e2d103f38a4d9f1
jivan patil <jivan@tripaneer.com>
2022-02-22 22:48:56 +0100

    doc update

--- src/arrays/slidingwindow/ReverseWordsinaString.java
+++ src/arrays/slidingwindow/ReverseWordsinaString.java
@@ -24,19 +24,14 @@ public class ReverseWordsinaString {
 
     public void reverseWords(char[] s) {
         reverseWord(s, 0 , s.length - 1 );
-
         int p1 = 0 , p2 = 0 , length = s.length ;
-
         while (p2 < length){
-
             while ( p2 < length && s[p2] != ' ')
                 p2++;
-
             reverseWord(s , p1 , p2 - 1);
             p1 = p2+1;
             p2++;
         }
-        System.out.println(s);
     }
 
     @Test
