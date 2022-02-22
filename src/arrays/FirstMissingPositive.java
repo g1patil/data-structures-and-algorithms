cce0672f859f24cb10dcd1e7170dc14416246f64
jivan patil <jivan@tripaneer.com>
2022-02-22 22:48:20 +0100

    doc update

--- src/arrays/FirstMissingPositive.java
+++ src/arrays/FirstMissingPositive.java
@@ -4,6 +4,7 @@ import annotation.Platform;
 import annotation.Quality;
 import annotation.Site;
 import annotation.Stage;
+import org.junit.jupiter.api.Test;
 
 /**
  * @author g1patil
@@ -29,5 +30,47 @@ public class FirstMissingPositive {
         }
 
         return i;
+    }
+
+    public int firstMissingPositiveOptimal(int[] nums) {
+        int n = nums.length;
+
+        // Base case.
+        int contains = 0;
+        for (int i = 0; i < n; i++)
+            if (nums[i] == 1) {
+                contains++;
+                break;
+            }
+
+        if (contains == 0)
+            return 1;
+
+        for (int i = 0; i < n; i++)
+            if ((nums[i] <= 0) || (nums[i] > n))
+                nums[i] = 1;
+
+        for (int i = 0; i < n; i++) {
+            int a = Math.abs(nums[i]);
+            if (a == n)
+                nums[0] = - Math.abs(nums[0]);
+            else
+                nums[a] = - Math.abs(nums[a]);
+        }
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > 0)
+                return i;
+        }
+
+        if (nums[0] > 0)
+            return n;
+
+        return n + 1;
+    }
+
+    @Test
+    public void test_(){
+        int[] ints = new int[]{1,2,3,4,5};
+        System.out.println(firstMissingPositiveOptimal(ints));
     }
 }
