02464efd700bb0a4f39d81cf378ea57d3542f01d
jivan patil <jivan@tripaneer.com>
2022-02-28 15:46:51 +0100

    270. Closest Binary Search Tree Value - final

---
+++ src/trees/binary/ClosestBinarySearchTreeValue.java
@@ -0,0 +1,37 @@
+package trees.binary;
+
+import annotation.Platform;
+import annotation.Quality;
+import annotation.Site;
+import annotation.Stage;
+import data.TreeNode;
+
+/**
+ * @author g1patil
+ * 270. Closest Binary Search Tree Value
+ */
+@Quality(Stage.TESTED)
+@Platform(Site.LEETCODE)
+public class ClosestBinarySearchTreeValue {
+
+    double diff = Integer.MAX_VALUE;
+    int result;
+
+    public int closestValue(TreeNode root, double target) {
+        closestValueHelper(root , target);
+        return result;
+    }
+
+    public void closestValueHelper(TreeNode root, double target){
+        if (root == null)
+            return;
+
+        double tempDiff = Math.abs(root.val - target);
+        if (tempDiff < diff){
+            diff = tempDiff;
+            result = root.val;
+        }
+        closestValueHelper(root.left , target);
+        closestValueHelper(root.right , target);
+    }
+}
