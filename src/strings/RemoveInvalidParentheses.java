a70717f54851faa0268b250a32216beeb963adef
jivan patil <jivan@tripaneer.com>
2022-03-02 13:15:36 +0100

    301. Remove Invalid Parentheses  - final

--- src/strings/RemoveInvalidParentheses.java
+++ src/strings/RemoveInvalidParentheses.java
@@ -18,26 +18,31 @@ import java.util.List;
 public class RemoveInvalidParentheses {
 
     public List<String> removeInvalidParentheses(String s) {
-        List<String> ans = new ArrayList<>();
-        remove(s, ans, 0, 0, new char[]{'(', ')'});
-        return ans;
+        List<String> result  = new ArrayList();
+        remove(s , result , 0 , 0 , new char[]{'(',')'});
+        return result;
     }
 
     public void remove(String s, List<String> ans, int last_i, int last_j,  char[] par) {
-        for (int stack = 0, i = last_i; i < s.length(); ++i) {
-            if (s.charAt(i) == par[0]) stack++;
-            if (s.charAt(i) == par[1]) stack--;
-            if (stack >= 0) continue;
-            for (int j = last_j; j <= i; ++j)
-                if (s.charAt(j) == par[1] && (j == last_j || s.charAt(j - 1) != par[1]))
-                    remove(s.substring(0, j) + s.substring(j + 1, s.length()), ans, i, j, par);
+        for (int i = last_i , stack = 0; i < s.length(); i++) {
+            if(s.charAt(i) == par[0])
+                stack++;
+            if (s.charAt(i) == par[1])
+                stack--;
+            if (stack >=0)
+                continue;
+
+            for (int j = last_j; j <= i ; j++) {
+                if(s.charAt(j) == par[1] && (j == last_j || s.charAt(j-1) != par[1] ) )
+                    remove(s.substring(0 , j) + s.substring(j+1) , ans , i , j , par);
+            }
+
             return;
         }
         String reversed = new StringBuilder(s).reverse().toString();
-        if (par[0] == '(') // finished left to right
-            remove(reversed, ans, 0, 0, new char[]{')', '('});
-        else // finished right to left
-            ans.add(reversed);
+        if (par[0] == '(')
+            remove( reversed , ans , 0,0, new char[]{')','('});
+        else ans.add(reversed);
     }
 
     @Test
