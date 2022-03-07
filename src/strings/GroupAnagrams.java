4d3ba5febadb8aea78b9f774c4c3dff4f0c1c7b5
jivan patil <jivan@tripaneer.com>
2022-03-07 21:38:49 +0100

    49. Group Anagrams - final

--- src/strings/GroupAnagrams.java
+++ src/strings/GroupAnagrams.java
@@ -1,5 +1,9 @@
 package strings;
 
+import annotation.Platform;
+import annotation.Quality;
+import annotation.Site;
+import annotation.Stage;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import java.util.*;
@@ -27,43 +31,48 @@ import java.util.*;
  * Input: strs = ["a"]
  * Output: [["a"]]
  * */
+@Quality(Stage.TESTED)
+@Platform(Site.LEETCODE)
 public class GroupAnagrams {
 
     /**
-     * Function to return the list of list of strings. Each list will be group of anagrams.
+     * Function to return the list of strings. Each list will be group of anagrams.
      * Combined all the lists and return list of lists
      * @param strs input strings
      * @return returns list of lists. Each list in that is group of anagram.
      * */
     public List<List<String>> groupAnagrams(String[] strs) {
 
-        List<List<String>> listList = new ArrayList<List<String>>();
         Map<String,List<String>> listMap = new HashMap<>();
         for (String string : strs){
+            int[] freq = new int[26];
 
-
-            char[] characters =  string.trim().replaceAll("\\s","").toCharArray();
-            Arrays.sort(characters);
-            String key = String.valueOf( characters);
-            if( listMap.containsKey(key) ){
-                listMap.get( key ).add( string );
-            } else {
-                List<String> o = new ArrayList<>();
-                o.add(string);
-                listMap.put( key , o);
+            for(char c : string.toCharArray()){
+                freq[c - 'a']++;
             }
+            StringBuilder sb = new StringBuilder();
+            for(int i : freq){
+                sb.append("#");
+                sb.append(i);
+            }
+            String key = sb.toString();
 
+            if (listMap.containsKey(key)){
+                listMap.get(key).add(string);
+            }else {
+                List<String> list = new ArrayList<>();
+                list.add(string);
+                listMap.put(key , list);
+            }
         }
 
-        listMap.keySet().forEach(key-> listList.add(listMap.get(key)));
-        return listList;
+        return new ArrayList(listMap.values());
     }
 
     @Test
     public void test(){
-        List<List<String>> result = groupAnagrams(new String[]{"ab","ba"});
-        Assertions.assertNotNull(result);
-        Assertions.assertEquals(1,result.size());
+        List<List<String>> result = groupAnagrams(new String[]{"bdddddddddd","bbbbbbbbbbc"});
+        System.out.println(result);
     }
 
     @Test
