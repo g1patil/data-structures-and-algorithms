28399a4ff66b8e6b32cc7723e3c6ac37de5e7900
jivan patil <jivan@tripaneer.com>
2022-03-08 23:48:19 +0100

    docs

---
+++ src/strings/LongestPalindromebyConcatenatingTwoLetterWords.java
@@ -0,0 +1,47 @@
+package strings;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author g1patil
+ */
+public class LongestPalindromebyConcatenatingTwoLetterWords {
+
+    public int longestPalindrome(String[] strs) {
+        Map<String, List<String>> listMap = new HashMap<>();
+        for (String string : strs){
+            int[] freq = new int[26];
+
+            for(char c : string.toCharArray()){
+                freq[c - 'a']++;
+            }
+            StringBuilder sb = new StringBuilder();
+            for(int i : freq){
+                sb.append("#");
+                sb.append(i);
+            }
+            String key = sb.toString();
+
+            if (listMap.containsKey(key)){
+                listMap.get(key).add(string);
+            }else {
+                List<String> list = new ArrayList<>();
+                list.add(string);
+                listMap.put(key , list);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        listMap.keySet().stream().forEach( k->{
+            List<String> list = listMap.get(k);
+
+            if (list.size() == 2){
+
+            }
+        });
+
+    }
+}
