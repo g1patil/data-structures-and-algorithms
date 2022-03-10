3b9a5d7ad3a7c7f9e5a8c37dce3ee2a346acf915
jivan patil <jivan@tripaneer.com>
2022-03-10 11:38:07 +0100

    1937. Maximum Number of Points with Cost - final

--- src/dynamicprogramming/MaximumNumberofPointswithCost.java
+++ src/dynamicprogramming/MaximumNumberofPointswithCost.java
@@ -22,37 +22,27 @@ public class MaximumNumberofPointswithCost {
         }
         for (int i = 0; i < row- 1 ; i++) {
 
-            long[] left = new long[values.length], right = new long[values.length];
-            left[0] = dp[0]; right[col-1] = dp[col-1];
+            long[] left = new long[col], right = new long[col];
 
-            for (int j = 1; j < left.length; j++)
+            left[0] = dp[0];
+            right[col-1] = dp[col-1];
+
+            for (int j = 1; j < col ; j++) {
                 left[j] = Math.max( left[j-1] - 1 , dp[j]);
+            }
 
-            for (int j = right.length - 2 ; j >= 0; j--)
+            for (int j = col - 2 ; j >= 0; j--) {
                 right[j] = Math.max( right[j+1] - 1 , dp[j]);
+            }
 
-            for (int j = 0; j < dp.length; j++)
+            for (int j = 0; j < dp.length; j++) {
                 dp[j] = values[i+1][j] + Math.max(left[j] , right[j]);
+            }
         }
-        return -1;
-    }
 
-    public long getLeft(int[][] dp , int row , int col){
-        long current = 0 , result = 0 ;
-
-        for (int i = 0; i < col; i++) {
-            result = Math.max( result , current + dp[row][i]);
-            current = Math.max( current , dp[row][i]) - 1 ;
-        }
-        return result;
-    }
-
-    public long getRight(int[][] dp , int row , int col){
-        long current = 0 , result = 0 ;
-
-        for (int i = dp[0].length - 1 ; i > col; i--) {
-            result = Math.max( result , current + dp[row][i]);
-            current = Math.max( current , dp[row][i]) - 1 ;
+        long result = 0;
+        for(long n : dp){
+            result = Math.max(n , result);
         }
         return result;
     }
