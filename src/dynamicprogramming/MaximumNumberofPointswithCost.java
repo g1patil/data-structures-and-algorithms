673ceae490b556e802caabb121e261dbf7b7dfa5
jivan patil <jivan@tripaneer.com>
2022-03-10 11:33:00 +0100

    1937. Maximum Number of Points with Cost - final

--- src/dynamicprogramming/MaximumNumberofPointswithCost.java
+++ src/dynamicprogramming/MaximumNumberofPointswithCost.java
@@ -1,33 +1,59 @@
 package dynamicprogramming;
 
+import annotation.Platform;
+import annotation.Quality;
+import annotation.Site;
+import annotation.Stage;
+
 /**
  * @author g1patil
+ * 1937. Maximum Number of Points with Cost
  */
+@Quality(Stage.TESTED)
+@Platform(Site.LEETCODE)
 public class MaximumNumberofPointswithCost {
 
-    public long maxPoints(int[][] P) {
-        int m = P.length, n = P[0].length;
-        long[] pre = new long[n];
-        for (int i = 0; i < n; ++i)
-            pre[i] = P[0][i];
-        for (int i = 0; i < m - 1; ++i){
-            long[] lft = new long[n], rgt = new long[n], cur = new long[n];
+    public long maxPoints(int[][] values) {
+        int row = values.length,col = values[0].length;
+        long[] dp = new long[col];
 
-            lft[0] = pre[0];
-            rgt[n - 1] = pre[n - 1];
-
-            for (int j = 1; j < n; ++j)
-                lft[j] = Math.max(lft[j - 1] - 1, pre[j]);
-            for (int j = n - 2; j >= 0; --j)
-                rgt[j] = Math.max(rgt[j + 1] - 1, pre[j]);
-            for (int j = 0; j < n; ++j)
-                cur[j] = P[i + 1][j] + Math.max(lft[j], rgt[j]);
-
-            pre = cur;
+        for (int i = 0; i < col; i++) {
+            dp[i] = values[0][i];
         }
-        long ans = 0;
-        for (int i = 0; i < n; ++i)
-            ans = Math.max(ans, pre[i]);
-        return ans;
+        for (int i = 0; i < row- 1 ; i++) {
+
+            long[] left = new long[values.length], right = new long[values.length];
+            left[0] = dp[0]; right[col-1] = dp[col-1];
+
+            for (int j = 1; j < left.length; j++)
+                left[j] = Math.max( left[j-1] - 1 , dp[j]);
+
+            for (int j = right.length - 2 ; j >= 0; j--)
+                right[j] = Math.max( right[j+1] - 1 , dp[j]);
+
+            for (int j = 0; j < dp.length; j++)
+                dp[j] = values[i+1][j] + Math.max(left[j] , right[j]);
+        }
+        return -1;
+    }
+
+    public long getLeft(int[][] dp , int row , int col){
+        long current = 0 , result = 0 ;
+
+        for (int i = 0; i < col; i++) {
+            result = Math.max( result , current + dp[row][i]);
+            current = Math.max( current , dp[row][i]) - 1 ;
+        }
+        return result;
+    }
+
+    public long getRight(int[][] dp , int row , int col){
+        long current = 0 , result = 0 ;
+
+        for (int i = dp[0].length - 1 ; i > col; i--) {
+            result = Math.max( result , current + dp[row][i]);
+            current = Math.max( current , dp[row][i]) - 1 ;
+        }
+        return result;
     }
 }
