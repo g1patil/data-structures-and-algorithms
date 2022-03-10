c951ad5f8137b90d13445bd9a15bcc812e5d1f82
jivan patil <jivan@tripaneer.com>
2022-03-10 23:27:19 +0100

    doc

--- src/trees/StepByStepDirectionsFromBTreeNodetoAnother.java
+++ src/trees/StepByStepDirectionsFromBTreeNodetoAnother.java
@@ -52,6 +52,6 @@ public class StepByStepDirectionsFromBTreeNodetoAnother {
         root.setChild(n1,n2);
         n1.setChild(n3,n4);
         n2.setChild(n5,n6);
-        System.out.println(getPath(root , 2 ));
+        System.out.println(getPath(root , 2,4 ));
     }
 }
