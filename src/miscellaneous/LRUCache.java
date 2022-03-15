b45852daa436d4c3dfeefda9cf79dbee154a4263
jivan patil <jivan@tripaneer.com>
2022-03-16 00:12:15 +0100

    146. LRU Cache - final

--- src/miscellaneous/LRUCache.java
+++ src/miscellaneous/LRUCache.java
@@ -13,21 +13,76 @@ import java.util.Map;
  * @author g1patil
  * 146. LRU Cache
  */
-@Quality(value = Stage.FAILING , details = "s**t LC. Passed the test that failed on the platform online. Passes 14/22 . No idea why Lc is behaving like this.")
+@Quality(value = Stage.TESTED)
 @Platform(Site.LEETCODE)
 public class LRUCache {
 
+    int size , capacity;
+    Map<Integer,Node> cache;
+    Node head , tail ;
     public LRUCache(int capacity) {
+        this.capacity = capacity;
+        cache = new HashMap();
+        head = new Node(-1,-1);
+        tail = new Node(-1,-1);
 
+        head.next = tail;
+        tail.pre = head;
     }
 
-    public int get(int key) {
+    public void delete(Node node){
+        node.pre.next = node.next;
+        node.next.pre = node.pre;
+    }
 
+    public void addToHead(Node node){
+        node.next = head.next;
+        head.next.pre = node;
+        node.pre = head;
+        head.next = node;
+    }
+    public int get(int key) {
+        if(cache.containsKey(key)){
+            Node node = cache.get(key);
+            delete(node);
+            addToHead(node);
+            return node.val;
+        }
+        return -1;
     }
 
     public void put(int key, int value) {
+        if(cache.containsKey(key)){
+            Node node = cache.get(key);
+            node.val = value;
+            delete(node);
+            addToHead(node);
+        } else {
+            Node node = new Node(key,value);
+            cache.put(key , node);
 
+            if(size < capacity){
+                size++;
+                addToHead(node);
+            } else {
+                cache.remove(tail.pre.key);
+                delete(tail.pre);
+                addToHead(node);
+            }
+        }
     }
 
+    private class Node {
+        int key;
+        int val;
+        Node next;
+        Node pre;
+
+        public Node(int key , int val){
+            this.key = key;
+            this.val = val;
+        }
+    }
 
 }
+
