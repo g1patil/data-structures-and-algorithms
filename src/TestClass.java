a68e3256989989876cd6f7ccd97fef14b97dc661
jivan patil <jivan@tripaneer.com>
2022-03-17 19:27:25 +0100

    test doc

--- src/TestClass.java
+++ src/TestClass.java
@@ -8,46 +8,75 @@ import org.junit.jupiter.api.Test;
 public class TestClass {
 
 
-    public int getMaxSum(TreeNode root){
-        if (root == null)
-            return 0;
+    static void factorial(int n)
+    {
+        int res[] = new int[500];
 
-        return root.val + Math.max( getMaxSum(root.left) , getMaxSum(root.right));
+        // Initialize result
+        res[0] = 1;
+        int res_size = 1;
+
+        // Apply simple factorial formula
+        // n! = 1 * 2 * 3 * 4...*n
+        for (int x = 2; x <= n; x++)
+            res_size = multiply(x, res, res_size);
+
+        System.out.println("Factorial of given number is ");
+        for (int i = res_size - 1; i >= 0; i--)
+            System.out.print(res[i]);
     }
 
-    @Test
-    public void test_(){
-        TreeNode root = new TreeNode(5);
-        TreeNode n1 = new TreeNode(6);
-        TreeNode n2 = new TreeNode(7);
-        TreeNode n3 = new TreeNode(1);
-        TreeNode n4 = new TreeNode(2);
-        n1.setChild(n3,n4);
-        root.setChild(n1,n2);
+    // This function multiplies x with the number
+    // represented by res[]. res_size is size of res[] or
+    // number of digits in the number represented by res[].
+    // This function uses simple school mathematics for
+    // multiplication. This function may value of res_size
+    // and returns the new value of res_size
+    static int multiply(int x, int res[], int res_size)
+    {
+        int carry = 0; // Initialize carry
 
-        System.out.println(getMaxSum(root));
-    }
-
-    public String compress(String input){
-        int index = 0 , count = 0;
-        StringBuilder sb = new StringBuilder();
-        char currentChar;
-        while ( index < input.length()){
-            count = 0;
-            currentChar = input.charAt(index);
-            while (index < input.length() && input.charAt(index) == currentChar){
-                count++;
-                index++;
-            }
-            sb.append(currentChar).append(count);
-
+        // One by one multiply n with individual
+        // digits of res[]
+        for (int i = 0; i < res_size; i++)
+        {
+            int prod = res[i] * x + carry;
+            res[i] = prod % 10; // Store last digit of
+            // 'prod' in res[]
+            carry = prod/10; // Put rest in carry
         }
-        return sb.toString();
+
+        // Put carry in res and increase result size
+        while (carry!=0)
+        {
+            res[res_size] = carry % 10;
+            carry = carry / 10;
+            res_size++;
+        }
+        return res_size;
     }
 
-    @Test
-    public void test_2(){
-        System.out.println(compress("aaabbc"));
+    static int multiplyMy(int x, int res[], int res_size) {
+        int carry = 0 , prod = 0 ;
+        for (int i = 0; i < res_size; i++) {
+            prod = res[i]*x + carry;
+            res[i] = prod % 10;
+            carry = prod/10;
+        }
+        while (carry!=0){
+            res[res_size] = carry % 10;
+            carry = carry / 10 ;
+            res_size++;
+        }
+        return res_size;
+    }
+
+
+
+    // Driver program
+    public static void main(String args[])
+    {
+        factorial(100);
     }
 
 
