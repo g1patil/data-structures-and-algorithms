936ed73e34c306fe0f64ecadac0841a492717cc1
jivan <jivan@tripaneer.com>
2022-03-31 23:24:09 +0200

    257. Binary Tree Paths - final

---
+++ src/trees/binary/BinaryTreePaths.java
@@ -0,0 +1,38 @@
+package trees.binary;
+
+import annotation.Platform;
+import annotation.Quality;
+import annotation.Site;
+import annotation.Stage;
+import data.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 257. Binary Tree Paths
+ * */
+@Quality(Stage.TESTED)
+@Platform(Site.LEETCODE)
+public class BinaryTreePaths {
+
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> result = new ArrayList();
+        dfs(root , result , "");
+        return result;
+    }
+
+    public void dfs(TreeNode root,List<String> result , String current){
+
+        if(root.left!=null)
+            dfs(root.left , result , current + root.val + "->");
+
+        if(root.right!=null)
+            dfs(root.right , result , current + root.val + "->");
+
+        if(root.left == null && root.right == null)
+            result.add(current + root.val);
+
+    }
+
+}
