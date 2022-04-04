5597663d13236b9feace2a42a3c7c71555548dd8
jivan <jivan@tripaneer.com>
2022-04-04 23:41:18 +0200

    993. Cousins in Binary Tree - final

--- src/trees/binary/CousinsinBinaryTree.java
+++ src/trees/binary/CousinsinBinaryTree.java
@@ -10,7 +10,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 /**
- * 993. Cousins in Binary Tree
+ * 993. Cousins in Binary Tre
  * */
 @Quality(Stage.FAILING)
 @Platform(Site.LEETCODE)
