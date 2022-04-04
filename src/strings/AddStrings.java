b1e8672a59bb52b4e95ecf8c624f4da2d4365334
jivan <jivan@tripaneer.com>
2022-04-04 23:41:05 +0200

    docs

--- src/strings/AddStrings.java
+++ src/strings/AddStrings.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.Test;
  * 415. Add Strings
  *
  * Given two non-negative integers num1 and num2 represented as string, return the sum of num1 and num2.
-
 Note:
 
 The length of both num1 and num2 is < 5100.
