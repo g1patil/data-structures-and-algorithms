dff557ce1eff28bf15da016000e8ae15be03f703
jivan <jivan@tripaneer.com>
2022-04-07 23:52:40 +0200

    387. First Unique Character in a String - failing

--- src/strings/FirstUniqueCharacterinaString.java
+++ src/strings/FirstUniqueCharacterinaString.java
@@ -1,11 +1,26 @@
 package strings;
 
+import annotation.Platform;
+import annotation.Site;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 387. First Unique Character in a String
  * */
+@Platform(Site.LEETCODE)
 public class FirstUniqueCharacterinaString {
 
     public int firstUniqChar(String s) {
+        Map<Character,Integer> characterIntegerMap = new HashMap<>();
 
+        for (int i = 0; i < s.length(); i++) {
+            char c  = s.charAt(i);
+            if (characterIntegerMap.containsKey(c))
+                return characterIntegerMap.get(c);
+            else characterIntegerMap.put(c, i);
+        }
+        return -1;
     }
 }
