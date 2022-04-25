424c6d2d3f89ec590ae88fd2ec85893aada7b0d8
Jivan <patil.jivan99@gmail.com>
2022-04-26 00:13:24 +0200

    2215. Find the Difference of Two Arrays - clean short code - improved . Referece : https://leetcode.com/problems/find-the-difference-of-two-arrays/discuss/1888950/Java-HashSet-Solution

---
+++ src/Bank/BankService.java
@@ -0,0 +1,71 @@
+package Bank;
+
+import lombok.NonNull;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BankService {
+
+    Map<String,Integer> bankToInternalStore = new HashMap<>();
+    Map<String,List<String>> bankToAccountMStore = new HashMap<>();
+
+    int accountToProcess = 20;
+
+    /**
+     * init banks
+     * */
+    public void initializeBanks(@NonNull String bankName , @NonNull List<String> account , int interval){
+        bankToInternalStore.put(bankName , interval);
+        bankToAccountMStore.put(bankName,account);
+    }
+
+    public Map<String,List<String>> getAccountToUpdate( int interval){
+        if (interval < 0)
+            return new HashMap<>();
+        Map<String,List<String>> accountToUpdate = new HashMap<>();
+
+        for (Map.Entry<String,Integer> entry : bankToInternalStore.entrySet()){
+            int numberOfAccount = bankToAccountMStore.get(entry.getKey()).size();
+            int bucketSize = numberOfAccount/bankToInternalStore.get(entry.getKey());
+            System.out.println(bucketSize);
+
+            int startingIndex = interval % entry.getValue() * bucketSize ;
+            List<String> accounts = bankToAccountMStore.get(entry.getKey())
+                    .subList(startingIndex ,  Math.min(startingIndex + bucketSize , bankToAccountMStore.get(entry.getKey()).size() ));
+                bankToAccountMStore.get(entry.getKey());
+                accountToUpdate.put( entry.getKey() , accounts);
+
+        }
+        return accountToUpdate;
+    }
+
+    @Test
+    public void test(){
+        String bank1 = "Chase";
+        List<String> account1 = new ArrayList<>();
+        account1.add("c12");
+        account1.add("c13");
+        account1.add("c14");
+        account1.add("c15");
+        account1.add("c16");
+        account1.add("c17");
+
+        String bank2 = "BoFa";
+        List<String> account2 = new ArrayList<>();
+        account2.add("b12");
+        account2.add("b12");
+
+        BankService bankService = new BankService();
+        bankService.initializeBanks(bank1 , account1 , 2);
+        bankService.initializeBanks(bank2 , account2 , 20);
+
+        System.out.println(bankService.getAccountToUpdate(1));
+
+
+    }
+}
--- src/arrays/FindtheDifferenceofTwoArrays.java
+++ src/arrays/FindtheDifferenceofTwoArrays.java
@@ -1,17 +1,17 @@
 package arrays;
 
 import annotation.Platform;
+import annotation.Quality;
 import annotation.Site;
+import annotation.Stage;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * 2215. Find the Difference of Two Arrays
  * */
+@Quality(Stage.TESTED)
 @Platform(Site.LEETCODE)
 public class FindtheDifferenceofTwoArrays {
 
@@ -53,6 +53,30 @@ public class FindtheDifferenceofTwoArrays {
         result.add(r2);
 
         return result;
+    }
+
+    /**
+     * Reference : https://leetcode.com/problems/find-the-difference-of-two-arrays/discuss/1888950/Java-HashSet-Solution
+     * */
+    public List<List<Integer>> findDifferenceImproved(int[] nums1, int[] nums2) {
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+        for (int num : nums1) {
+            set1.add(num);
+        }
+        for (int num : nums2) {
+            set2.add(num);
+            set1.remove(num);
+        }
+
+        for (int num : nums1) {
+            set2.remove(num);
+        }
+
+        List<List<Integer>> ans = new ArrayList<>();
+        ans.add(new ArrayList<>(set1));
+        ans.add(new ArrayList<>(set2));
+        return ans;
     }
 
     @Test
