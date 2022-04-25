5e4f801df4c8e9b513e5b6b99bd9903d96c8fd3a
Jivan <patil.jivan99@gmail.com>
2022-04-25 23:32:43 +0200

    1213. Intersection of Three Sorted Arrays - WIP

---
+++ src/arrays/IntersectionThreeSortedArrays.java
@@ -0,0 +1,24 @@
+package arrays;
+
+import annotation.Platform;
+import annotation.Site;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 1213. Intersection of Three Sorted Arrays
+ * */
+@Platform(Site.LEETCODE)
+public class IntersectionThreeSortedArrays {
+    public List<Integer> arraysIntersection(int[] arr1, int[] arr2, int[] arr3) {
+        List<Integer> result = new ArrayList<>();
+        return result;
+    }
+
+    @Test
+    public void test(){
+
+    }
+}
