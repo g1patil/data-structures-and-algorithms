51a1703e62d8bcdae7e122eb347f6bbe7e52c8b9
jivan <jivan@tripaneer.com>
2022-04-26 16:36:28 +0200

    staging changes

--- src/SomeTestClass.java
+++ src/SomeTestClass.java
@@ -1,99 +1,55 @@
-import lombok.NonNull;
-
-import java.time.temporal.ChronoUnit;
-import java.util.*;
-
 /**
- * @author g1patil
+ Problem  :
+ Given the string with repeating chars , remove the repeating chars when the
+ consecutive count for any character goes more than 2.
+
+ Example :
+ input : aaaaaaaabbbbbcccc
+ output : aabbcc
+
+ input abbbbbbvvvvvv
+ output : abbvv
+
+ input abc
+ output : abc
+
  */
 public class SomeTestClass {
-    private static final int MIN_TRANSACTIONS = 3 ;
-    private static final double VARIATION_PERCENTAGE = 0.2 ;
 
-    @Override
-    public List<String> getCompaniesWithRecurringTransactions(Transaction[] transactions) {
-        Preconditions.checkArgument(transactions!=null && transactions.length > 0 , "Invalid Input");
+    public static String removeRepeatingChars(String input){
 
-        List<String> companies = new ArrayList<>();
-        Map<String, List<Transaction>> transactionsMap = buildTransactionMap(transactions);
+        int start = 0;
+        int end = 0;
 
-        for(String company : transactionsMap.keySet()) {
-            List<Transaction> listOfTransactions = transactionsMap.get(company);
-            if(listOfTransactions.size() < MIN_TRANSACTIONS) {
-                continue;
+        String result = "";
+
+        while (end < input.length()) {
+            System.out.println("Start: " + start);
+            System.out.println("End: " + end);
+            char startCharacter = input.charAt(start);
+            System.out.println("StartChar: " + startCharacter);
+            char endCharacter  = input.charAt(end);
+            System.out.println("endChar: " + endCharacter);
+            if (end - start >= 2 && startCharacter == endCharacter ) {
+                end++;
+            } else {
+                if (startCharacter == endCharacter) {
+                    result +=   startCharacter;
+                    end++;
+                }else {
+                    // result += startCharacter;
+                    start = end;
+                    end = start+1;
+                }
             }
-
-            if(hasRecurringTransactionsVariation(listOfTransactions)) {
-                companies.add(company);
-            }
+            System.out.println("Result:" + result);
         }
-        return companies;
-    }
-
-    @Override
-    public Set<Transaction> getConcurrentTransactions(@NonNull final List<Transaction> transactions){
-        if (transactions.isEmpty())
-            return new HashSet<>();
-
-        Set<Transaction> result = new HashSet<>();
-        Collections.sort(transactions , Comparator.comparing(Transaction::getTimestamp));
-        for (int i = 0; i < transactions.size() - 1; i++) {
-            if (transactions.get(i).getTimestamp().equals(transactions.get(i+1).getTimestamp())){
-                result.add(transactions.get(i));
-                result.add(transactions.get(i+1));
-            }
-        }
+        // result += input.charAt(end-1);
         return result;
     }
-
-    private Map<String, List<Transaction>> buildTransactionMap(final Transaction[] transactions){
-        Map<String, List<Transaction>> transactionsMap = new HashMap<>();
-        for(Transaction t : transactions) {
-            List<Transaction> listOfTransactions = transactionsMap.getOrDefault(t.getName().toLowerCase(), new ArrayList<>());
-            listOfTransactions.add(t);
-            transactionsMap.put(t.getName(), listOfTransactions);
-        }
-        transactionsMap.keySet().stream().forEach(t->{
-            transactionsMap.get(t).sort(Comparator.comparing(o -> o.getTimestamp()));
-        });
-        return transactionsMap;
-    }
-
-
-    public boolean hasRecurringTransactionsNoVariation(List<Transaction> transactions){
-        Long previousDifference = 0L ;
-        Transaction previousTransaction = transactions.get(0);
-
-        for (int i = 1; i < transactions.size(); i++) {
-            Transaction currentTransaction = transactions.get(i);
-            Long currentDiff = ChronoUnit.DAYS.between(previousTransaction.getTimestamp() , currentTransaction.getTimestamp());
-
-            if (currentDiff.equals(previousDifference) && currentTransaction.getAmount() == previousTransaction.getAmount()){
-                return true;
-            } else {
-                previousTransaction = currentTransaction;
-                previousDifference = currentDiff;
-            }
-        }
-        return false;
-    }
-    public boolean hasRecurringTransactionsVariation(List<Transaction> transactions) {
-        double amountVariation = 0 ;
-        Queue<Transaction> transactionQueue = new PriorityQueue<>(Comparator.comparingDouble(o->o.getAmount()));
-
-        for (Transaction transaction : transactions){
-            if (transactionQueue.isEmpty() || (transaction.getAmount() <= amountVariation)){
-                transactionQueue.add(transaction);
-            } else {
-                transactionQueue.poll();
-                transactionQueue.add(transaction);
-            }
-            amountVariation = transactionQueue.peek().getAmount() + transactionQueue.peek().getAmount() * VARIATION_PERCENTAGE;
-
-            if (transactionQueue.size() >= 3){
-                System.out.println(transactionQueue);
-            }
-        }
-        return false;
+    public static void main(String[] args) {
+        System.out.println( removeRepeatingChars("aaaaaaaabbbbbcccc"));
+        System.out.println( removeRepeatingChars("abbbbbbvvvvvv") );
+        System.out.println( removeRepeatingChars("abc") );
     }
 }
