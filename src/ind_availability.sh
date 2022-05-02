aefddb74e47e0c5bbdd3ac93efbf493e490ea6de
jivan <jivan@tripaneer.com>
2022-05-02 16:02:41 +0200

    before certain date

--- src/ind_availability.sh
+++ src/ind_availability.sh
@@ -4,18 +4,31 @@
 declare -a array=("amsterdam" "Haag" "Rotterdam" "Zwolle" "Bosch" "Harlem")
 declare -a codes=("AM" "DH" "RO" "ZW" "DB" "fa24ccf0acbc76a7793765937eaee440")
 
+
 print () {
-    echo ${codes[$1]} 
-    curl -s https://oap.ind.nl/oap/api/desks/${codes[$1]}/slots/\?productKey\=BIO\&persons\=1 | tail +2 > ${array[$1]}
-    say "Date open for $(echo $(cat ${array[$1]} | jq '.data[0]' | jq  -r .date) |  { IFS=- read y m d && echo "$d/$m/$y"; } ) for ${array[$1]}"
-    rm ${array[$1]}
+    # echo ${codes[$1]} 
+    result=$(curl -s https://oap.ind.nl/oap/api/desks/${codes[$1]}/slots/\?productKey\=DOC\&persons\=1 | tail +2 );
+    avail=$(echo $(echo ${result} | jq '.data[0]' | jq  -r .date) |  { IFS=- read y m d && echo "$d/$m/$y"; } ) ;
+    # echo $avail;
+    # say "Date open for ${avail} for ${array[$1]}"
+
+    d2="2022-05-31" d1=$(echo ${avail} | awk -v FS=/ -v OFS=- '{print $3,$2,$1}')
+    if [ "$d1" = "$d2" ]; then
+        echo "same day"
+    elif expr "$d1" "<=" "$d2" >/dev/null; then
+        echo "${d1} is earlier than ${d2}"
+        say "Date open for ${avail} for ${array[$1]}"
+    else
+        echo "${d1} is later than ${d2}"
+    fi
+    # rm ${array[$1]}
 }
 
 while true
 do 
-    for i in {0..5}
+    for i in {0..0}
     do
        print $i
     done
-    sleep 20
+    sleep 2
 done
