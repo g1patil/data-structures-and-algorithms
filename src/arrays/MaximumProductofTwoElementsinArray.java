1a78810580a448fe27264ba68c6130a6e7afb6ae
Jivan <patil.jivan99@gmail.com>
2022-05-24 21:51:06 +0200

    1464. Maximum Product of Two Elements in an Array - WIP

---
+++ src/arrays/MaximumProductofTwoElementsinArray.java
@@ -0,0 +1,15 @@
+package arrays;
+
+import annotation.Platform;
+import annotation.Site;
+
+/**
+ * 1464. Maximum Product of Two Elements in an Array
+ * */
+@Platform(Site.LEETCODE)
+public class MaximumProductofTwoElementsinArray {
+
+    public int maxProduct(int[] nums) {
+
+    }
+}
