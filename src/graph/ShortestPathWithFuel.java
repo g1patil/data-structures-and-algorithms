49a6e4a88de4ac56980e77124cafdeee3d06d47c
Jivan <patil.jivan99@gmail.com>
2022-06-01 20:51:49 +0200

    Shortest path algo with complexity of the fuel. initial commit

--- src/graph/ShortestPathWithFuel.java
+++ src/graph/ShortestPathWithFuel.java
@@ -2,6 +2,8 @@ package graph;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.*;
+
 /**
  * Google Maps recently launched the fucntionality to get the\
  * route to the destination keeping in mind the fuel efficincy.
@@ -17,12 +19,51 @@ import org.junit.jupiter.api.Test;
 
 public class ShortestPathWithFuel {
 
-    private int getShortestPath(){
-        return -1;
+    Map<String,Integer> paths = new HashMap<>();
+    Set<String> fuelStations = new HashSet<>();
+
+
+    private int getShortestPath(Map<String, List<CityRoute>> graph , String source , String destination, final int fuelCapacity){
+        getShortestPathHelper(graph , source , destination , fuelCapacity ,0 );
+        return paths.get(destination);
+    }
+
+    private void getShortestPathHelper(Map<String, List<CityRoute>> graph , String source , String dest , int currentFuel , int dist){
+        if (source.equals(dest)){
+            if (paths.containsKey(source) && paths.get(source) > dist){
+                paths.put(source , dist);
+            } else paths.put(source , dist);
+        }
+
+        for (CityRoute nextSource : graph.get(source)){
+            int fuelCost = nextSource.fuelNeed;
+            int distCost = nextSource.distance;
+
+            if (currentFuel - fuelCost > 0 )
+                getShortestPathHelper(graph , nextSource.destination , dest , currentFuel - fuelCost , dist + distCost);
+        }
     }
 
     @Test
     public void test(){
+        Map<String, List<CityRoute>> graph = new HashMap<>();
+
+        List<CityRoute> forA = new ArrayList<>();
+        forA.add(new CityRoute(5 , 3 , "B"));
+        forA.add(new CityRoute(3 , 2 , "C"));
+
+        List<CityRoute> forB = new ArrayList<>();
+        forA.add(new CityRoute(4 , 3 , "D"));
+
+        List<CityRoute> forC = new ArrayList<>();
+        forA.add(new CityRoute(4 , 3 , "D"));
+
+        graph.put("A", forA);
+        graph.put("B", forB);
+        graph.put("C", forC);
+
+        System.out.println(getShortestPath(graph , "A","B",10));
+
 
     }
 }
@@ -30,13 +71,11 @@ public class ShortestPathWithFuel {
 class CityRoute {
     int distance;
     int fuelNeed;
-    String source;
     String destination;
 
-    public CityRoute(int distance, int fuelNeed, String source, String destination) {
+    public CityRoute(int distance, int fuelNeed, String destination) {
         this.distance = distance;
         this.fuelNeed = fuelNeed;
-        this.source = source;
         this.destination = destination;
     }
 }
