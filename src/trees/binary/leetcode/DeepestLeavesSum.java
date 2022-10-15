d576535b39571fe02c8ee75d12f58cc4d4eab360
Jivan <patil.jivan99@gmail.com>
2022-10-15 17:50:29 +0200

    1302. Deepest Leaves Sum - final tested

---
+++ src/trees/binary/leetcode/DeepestLeavesSum.java
@@ -0,0 +1,55 @@
+package trees.binary.leetcode;
+
+
+import model.TreeNode;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 1302. Deepest Leaves Sum
+ * */
+public class DeepestLeavesSum {
+
+    public int deepestLeavesSum(TreeNode root) {
+        int deepestLevel = 0;
+        Map<Integer,Integer> levelSum  = new HashMap();
+        deepestLevel = deepestLeavesSumHelper(root,deepestLevel,levelSum);
+        return levelSum.get(deepestLevel);
+    }
+
+    private int deepestLeavesSumHelper(TreeNode root,int level,Map<Integer,Integer> levelSum){
+        if (root == null)
+            return level - 1;
+
+        levelSum.put(level,levelSum.getOrDefault(level,0) + root.val);
+        return Math.max( deepestLeavesSumHelper(root.left , level + 1 , levelSum),
+                deepestLeavesSumHelper(root.right , level + 1 , levelSum)
+                );
+    }
+
+    @Test
+    public void test(){
+        TreeNode root = new TreeNode(10);
+        root.setChild(new TreeNode(5), new TreeNode(6));
+        System.out.println(deepestLeavesSum(root));
+    }
+
+    @Test
+    public void test2(){
+        TreeNode root = new TreeNode(5);
+        TreeNode n1 = new TreeNode(6);
+        TreeNode n2 = new TreeNode(7);
+        TreeNode n3 = new TreeNode(8);
+        TreeNode n4 = new TreeNode(9);
+        TreeNode n5 = new TreeNode(10);
+        TreeNode n6 = new TreeNode(11);
+
+        root.setChild(n1,n2);
+        n1.setChild(n3,null);
+        n2.setChild(null,null);
+
+        System.out.println(deepestLeavesSum(root));
+    }
+}
