034905556890418ecd7a18eed7bb579d25416cf6
Jivan <patil.jivan99@gmail.com>
2022-10-16 12:15:48 +0200

    230. Kth Smallest Element in a BST - WIP

---
+++ src/trees/binary/leetcode/KthSmallestElementInBST.java
@@ -0,0 +1,13 @@
+package trees.binary.leetcode;
+
+import model.TreeNode;
+
+/**
+ * 230. Kth Smallest Element in a BST
+ * */
+public class KthSmallestElementInBST {
+
+    public int kthSmallest(TreeNode root, int k) {
+
+    }
+}
