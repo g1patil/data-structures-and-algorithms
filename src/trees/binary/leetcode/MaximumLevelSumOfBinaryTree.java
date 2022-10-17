d481e43bcb70d1deb0e9ff459ae2d9e804717ece
Jivan <patil.jivan99@gmail.com>
2022-10-17 21:34:14 +0200

    1161. Maximum Level Sum of a Binary Tree - WIP

---
+++ src/trees/binary/leetcode/MaximumLevelSumOfBinaryTree.java
@@ -0,0 +1,15 @@
+package trees.binary.leetcode;
+import model.TreeNode;
+
+
+/**
+ * 1161. Maximum Level Sum of a Binary Tree
+ * */
+public class MaximumLevelSumOfBinaryTree {
+
+    public int maxLevelSum(TreeNode root) {
+        return -1;
+    }
+
+
+}
