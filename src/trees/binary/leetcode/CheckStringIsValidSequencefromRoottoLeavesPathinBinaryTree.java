9a968ed393360843e58de6eba0ff632105626f45
Jivan <patil.jivan99@gmail.com>
2022-10-18 21:33:19 +0200

    1430. Check If a String Is a Valid Sequence from Root to Leaves Path in a Binary Tree - WIP

---
+++ src/trees/binary/leetcode/CheckStringIsValidSequencefromRoottoLeavesPathinBinaryTree.java
@@ -0,0 +1,14 @@
+package trees.binary.leetcode;
+
+import model.TreeNode;
+
+/**
+ * 1430. Check If a String Is a Valid Sequence from Root to Leaves Path in a Binary Tree
+ * link : https://leetcode.com/problems/check-if-a-string-is-a-valid-sequence-from-root-to-leaves-path-in-a-binary-tree/
+ * */
+public class CheckStringIsValidSequencefromRoottoLeavesPathinBinaryTree {
+
+    public boolean isValidSequence(TreeNode root, int[] arr) {
+        return false;
+    }
+}
