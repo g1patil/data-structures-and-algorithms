38a5f736ef96b36ea7265af39527d603605d774b
Jivan <patil.jivan99@gmail.com>
2022-10-18 21:50:45 +0200

    1430. Check If a String Is a Valid Sequence from Root to Leaves Path in a Binary Tree - 53/63 failing

--- src/trees/binary/leetcode/CheckStringIsValidSequencefromRoottoLeavesPathinBinaryTree.java
+++ src/trees/binary/leetcode/CheckStringIsValidSequencefromRoottoLeavesPathinBinaryTree.java
@@ -12,7 +12,7 @@ import org.junit.jupiter.api.Test;
  * 1430. Check If a String Is a Valid Sequence from Root to Leaves Path in a Binary Tree
  * link : https://leetcode.com/problems/check-if-a-string-is-a-valid-sequence-from-root-to-leaves-path-in-a-binary-tree/
  * */
-@Quality(Stage.UNTESTED)
+@Quality(Stage.FAILING)
 @Platform(Site.LEETCODE)
 public class CheckStringIsValidSequencefromRoottoLeavesPathinBinaryTree {
 
@@ -21,7 +21,7 @@ public class CheckStringIsValidSequencefromRoottoLeavesPathinBinaryTree {
         for (int i : arr)
             sb.append(i);
 
-        return isValidSequenceHelper(root , String.valueOf(root.val) , sb.toString() );
+        return isValidSequenceHelper(root , "" , sb.toString() );
     }
 
     private boolean isValidSequenceHelper(TreeNode root, String s, String expected) {
@@ -31,8 +31,8 @@ public class CheckStringIsValidSequencefromRoottoLeavesPathinBinaryTree {
         if (root == null)
             return false;
 
-        return isValidSequenceHelper(root.left , s + String.valueOf(s) , expected) ||
-         isValidSequenceHelper(root.right , s + String.valueOf(s) , expected) ;
+        return isValidSequenceHelper(root.left , s + root.val, expected) ||
+         isValidSequenceHelper(root.right , s + root.val, expected) ;
     }
 
     @Test
@@ -50,6 +50,23 @@ public class CheckStringIsValidSequencefromRoottoLeavesPathinBinaryTree {
         n6.setChild(n5,n7);
 
         Assertions.assertTrue(isValidSequence(n4 , new int[]{4,2,1}));
+    }
+
+    @Test
+    public void test2(){
+        TreeNode n1 = new TreeNode(1);
+        TreeNode n2 = new TreeNode(2);
+        TreeNode n3 = new TreeNode(3);
+        TreeNode n4 = new TreeNode(4);
+        TreeNode n5 = new TreeNode(5);
+        TreeNode n6 = new TreeNode(6);
+        TreeNode n7 = new TreeNode(7);
+
+        n2.setChild(n1,n3);
+        n4.setChild(null,n6);
+        n6.setChild(n5,n7);
+
+        Assertions.assertTrue(isValidSequence(n4 , new int[]{4}));
     }
 
 
