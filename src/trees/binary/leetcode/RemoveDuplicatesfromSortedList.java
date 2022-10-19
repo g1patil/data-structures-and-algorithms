f35a67630c1295dcb2d4fb295fb8f00fd7b183ec
Jivan <patil.jivan99@gmail.com>
2022-10-19 22:03:44 +0200

    83. Remove Duplicates from Sorted List - WIP

---
+++ src/trees/binary/leetcode/RemoveDuplicatesfromSortedList.java
@@ -0,0 +1,16 @@
+package trees.binary.leetcode;
+
+import lists.ListNode;
+
+/**
+ * 83. Remove Duplicates from Sorted List
+ *
+ * Given the head of a sorted linked list, delete all duplicates
+ * such that each element appears only once. Return the linked list sorted as well.
+ * */
+public class RemoveDuplicatesfromSortedList {
+
+    public ListNode deleteDuplicates(ListNode head) {
+        return null;
+    }
+}
