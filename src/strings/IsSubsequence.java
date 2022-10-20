19791d8dbdd2ea50b3e3279dde11d668de68b9fc
Jivan <patil.jivan99@gmail.com>
2022-10-20 23:01:33 +0200

    392. Is Subsequence - wip

---
+++ src/strings/IsSubsequence.java
@@ -0,0 +1,15 @@
+package strings;
+
+import annotation.Platform;
+import annotation.Site;
+
+/**
+ * 392. Is Subsequence
+ * */
+@Platform(Site.LEETCODE)
+public class IsSubsequence {
+
+    public boolean isSubsequence(String s, String t) {
+        return false;
+    }
+}
