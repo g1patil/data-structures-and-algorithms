a32ea7def1588e537a5386e06b232dc88c8f41ea
Jivan <patil.jivan99@gmail.com>
2022-10-28 23:26:29 +0200

    654. Maximum Binary Tree - final

--- src/trees/binary/leetcode/MaxBinaryTree.java
+++ src/trees/binary/leetcode/MaxBinaryTree.java
@@ -5,6 +5,7 @@ import annotation.Quality;
 import annotation.Site;
 import annotation.Stage;
 import model.TreeNode;
+import org.junit.jupiter.api.Test;
 
 /**
  * 654. Maximum Binary Tree
@@ -16,12 +17,43 @@ import model.TreeNode;
  * Recursively build the right subtree on the subarray suffix to the right of the maximum value.
  * Return the maximum binary tree built from nums.
  * */
-@Quality(Stage.INCOMPLETE)
+@Quality(Stage.TESTED)
 @Platform(Site.LEETCODE)
 public class MaxBinaryTree {
 
 
     public TreeNode constructMaximumBinaryTree(int[] nums) {
-        return null;
+        return constructMaximumBinaryTreeHelper(0 , nums.length - 1  , nums);
+    }
+
+    public TreeNode constructMaximumBinaryTreeHelper(int start , int end , int[] nums) {
+        TreeNode root = null;
+
+        if (end < 0 || start >= nums.length || end < start)
+            return root;
+
+        int maxIndex = getMax(start,end,nums);
+        root = new TreeNode(nums[maxIndex]);
+
+        root.left = constructMaximumBinaryTreeHelper(start , maxIndex - 1  , nums);
+        root.right = constructMaximumBinaryTreeHelper(maxIndex + 1  ,  end , nums);
+        return root;
+    }
+
+    private int getMax(final int start , final int end , final int[] arr){
+        int max = Integer.MIN_VALUE;
+        int resultIndex = -1;
+        for (int i = start; i <= end; i++) {
+            if (arr[i] > max){
+                max = arr[i];
+                resultIndex = i ;
+            }
+        }
+        return resultIndex;
+    }
+
+    @Test
+    public void test(){
+        TreeNode.printTreeStatic(constructMaximumBinaryTree(new int[]{3,2,1,6,0,5}));
     }
 }
