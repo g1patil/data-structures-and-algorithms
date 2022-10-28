8441a5436037dae1d36cc6020a656bd4adccd19a
Jivan <patil.jivan99@gmail.com>
2022-10-28 23:01:50 +0200

    654. Maximum Binary Tree - WIP

---
+++ src/trees/binary/leetcode/MaxBinaryTree.java
@@ -0,0 +1,27 @@
+package trees.binary.leetcode;
+
+import annotation.Platform;
+import annotation.Quality;
+import annotation.Site;
+import annotation.Stage;
+import model.TreeNode;
+
+/**
+ * 654. Maximum Binary Tree
+ * You are given an integer array nums with no duplicates.
+ * A maximum binary tree can be built recursively from nums using the following algorithm:
+ *
+ * Create a root node whose value is the maximum value in nums.
+ * Recursively build the left subtree on the subarray prefix to the left of the maximum value.
+ * Recursively build the right subtree on the subarray suffix to the right of the maximum value.
+ * Return the maximum binary tree built from nums.
+ * */
+@Quality(Stage.INCOMPLETE)
+@Platform(Site.LEETCODE)
+public class MaxBinaryTree {
+
+
+    public TreeNode constructMaximumBinaryTree(int[] nums) {
+        return null;
+    }
+}
---
+++ src/trees/binary/leetcode/MaximumBinaryTree2.java
@@ -0,0 +1,10 @@
+package trees.binary.leetcode;
+
+import model.TreeNode;
+
+public class MaximumBinaryTree2 {
+
+    public TreeNode insertIntoMaxTree(TreeNode root, int val) {
+        return null;
+    }
+}
