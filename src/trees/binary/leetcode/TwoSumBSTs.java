7f2fd59ca93edc59985040b39ff9079e9b925241
Jivan <patil.jivan99@gmail.com>
2022-10-30 23:16:43 +0100

    1214. Two Sum BSTs - wip

---
+++ src/trees/binary/leetcode/TwoSumBSTs.java
@@ -0,0 +1,20 @@
+package trees.binary.leetcode;
+
+import annotation.Platform;
+import annotation.Site;
+import model.TreeNode;
+
+/**
+ * 1214. Two Sum BSTs
+ *
+ *Given the roots of two binary search trees, root1 and root2,
+ * return true if and only if there is a node in the first tree and a node in the
+ * second tree whose values sum up to a given integer target.
+ * */
+@Platform(Site.LEETCODE)
+public class TwoSumBSTs {
+
+    public boolean twoSumBSTs(TreeNode root1, TreeNode root2, int target) {
+        return false;
+    }
+}
