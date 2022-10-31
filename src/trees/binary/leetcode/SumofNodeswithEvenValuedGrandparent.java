4127b133ea5957777287b9705240f8ac3cd87070
Jivan <patil.jivan99@gmail.com>
2022-10-31 21:26:50 +0100

    1315. Sum of Nodes with Even-Valued Grandparent - doc

---
+++ src/trees/binary/leetcode/SumofNodeswithEvenValuedGrandparent.java
@@ -0,0 +1,23 @@
+package trees.binary.leetcode;
+
+import annotation.Platform;
+import annotation.Site;
+import model.TreeNode;
+
+/**
+ * 1315. Sum of Nodes with Even-Valued Grandparent
+ *
+ * Given the root of a binary tree,
+ * return the sum of values of nodes with an even-valued grandparent.
+ * If there are no nodes with an even-valued grandparent, return 0.
+ *
+ * A grandparent of a node is the parent of its parent if it exists.
+ * */
+
+@Platform(Site.LEETCODE)
+public class SumofNodeswithEvenValuedGrandparent {
+
+    public int sumEvenGrandparent(TreeNode root) {
+
+    }
+}
