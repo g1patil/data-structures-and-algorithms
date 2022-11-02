0cffbc6417bd19d096de5c7714dfd5da6ecdb399
Jivan <patil.jivan99@gmail.com>
2022-11-02 21:42:02 +0100

    1305. All Elements in Two Binary Search Trees - wip

---
+++ src/trees/binary/leetcode/AllElementsinTwoBinarySearchTrees.java
@@ -0,0 +1,19 @@
+package trees.binary.leetcode;
+
+
+import annotation.Platform;
+import annotation.Site;
+import model.TreeNode;
+
+import java.util.List;
+
+/**
+ * 1305. All Elements in Two Binary Search Trees
+ * */
+@Platform(Site.LEETCODE)
+public class AllElementsinTwoBinarySearchTrees {
+
+    public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
+        return null;
+    }
+}
