5307fab592d2d54c8c1536e178a694bf8446355c
Jivan <patil.jivan99@gmail.com>
2022-11-06 22:59:06 +0100

    1171. Remove Zero Sum Consecutive Nodes from Linked List - wip

---
+++ src/lists/RemoveZeroSumConsecutiveNodesLL.java
@@ -0,0 +1,11 @@
+package lists;
+
+/**
+ * 1171. Remove Zero Sum Consecutive Nodes from Linked List
+ * */
+public class RemoveZeroSumConsecutiveNodesLL {
+
+    public ListNode removeZeroSumSublists(ListNode head) {
+        return head;
+    }
+}
