01b245d1978c9db1383ad31d3677d0dd44675126
Jivan <patil.jivan99@gmail.com>
2022-11-06 23:01:54 +0100

    1171. Remove Zero Sum Consecutive Nodes from Linked List - test case

--- src/lists/RemoveZeroSumConsecutiveNodesLL.java
+++ src/lists/RemoveZeroSumConsecutiveNodesLL.java
@@ -1,5 +1,7 @@
 package lists;
 
+import org.junit.jupiter.api.Test;
+
 /**
  * 1171. Remove Zero Sum Consecutive Nodes from Linked List
  * */
@@ -7,5 +9,14 @@ public class RemoveZeroSumConsecutiveNodesLL {
 
     public ListNode removeZeroSumSublists(ListNode head) {
         return head;
+    }
+
+    @Test
+    public void test(){
+        ListNode n1 = new ListNode(1);
+        ListNode n2 = new ListNode(2);
+        ListNode n3 = new ListNode(-3);
+        ListNode n4 = new ListNode(3);
+        ListNode n5 = new ListNode(1);
     }
 }
