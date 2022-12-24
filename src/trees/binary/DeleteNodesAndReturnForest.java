5cae512fad6228b35c65729296046255bf182d95
Jivan <patil.jivan99@gmail.com>
2022-12-24 23:57:27 +0100

    1110. Delete Nodes And Return Forest - wrong approach

--- src/trees/binary/DeleteNodesAndReturnForest.java
+++ src/trees/binary/DeleteNodesAndReturnForest.java
@@ -11,50 +11,8 @@ import java.util.*;
 public class DeleteNodesAndReturnForest {
 
     public List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
-        Map<TreeNode, TreeNode> parentMap = new HashMap<>();
-        buildParentMap(parentMap,root);
-        Set<TreeNode> nodeSet = new HashSet<>();
-        returnForest(parentMap, nodeSet,root,to_delete);
-
-        return new LinkedList(nodeSet);
+        return new ArrayList<>();
     }
 
-    private void returnForest(Map<TreeNode, TreeNode> parentMap, Set<TreeNode> nodeSet, TreeNode root, int[] to_delete) {
-        if (root == null)
-            return;
 
-        returnForest(parentMap, nodeSet,root.left,to_delete);
-        if (contains(root.val , to_delete)) {
-            addToSet(parentMap,nodeSet,root);
-        }
-        returnForest(parentMap, nodeSet,root.right,to_delete);
-
-    }
-
-    private void addToSet(Map<TreeNode, TreeNode> parentMap, Set<TreeNode> nodeSet, TreeNode root) {
-        TreeNode parent = parentMap.get(root);
-        TreeNode left = root.left;
-        TreeNode right = root.right;
-
-        if (parent)
-    }
-
-    private void buildParentMap(Map<TreeNode, TreeNode> parentMap,TreeNode root) {
-        if (root == null || (root.left == null && root.right == null))
-            return;
-
-        buildParentMap(parentMap,root.left);
-        parentMap.put(root.left,root);
-        parentMap.put(root.right,root);
-        buildParentMap(parentMap,root.right);
-
-    }
-
-    private boolean contains(int target,int[] source){
-        for (int i : source) {
-            if (i == target)
-                return true;
-        }
-        return false;
-    }
 }
