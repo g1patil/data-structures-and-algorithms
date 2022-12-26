5fcc73b07ab4ed8efb379a1266d284a120c4dcdb
Jivan <patil.jivan99@gmail.com>
2022-12-26 10:11:24 +0100

    1331. Rank Transform of an Array - WIP

---
+++ src/sorting/RankTransformofArray.java
@@ -0,0 +1,23 @@
+package sorting;
+
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 1331. Rank Transform of an Array
+ * */
+public class RankTransformofArray {
+
+    public int[] arrayRankTransform(int[] arr) {
+        return arr;
+    }
+
+    @Test
+    public void test(){
+        System.out.println(arrayRankTransform(new int[]{37,12,28,9,100,56,80,5,12}));
+    }
+}
