71b401a33100d62f65bcd92cb7d7408b49c00e13
Jivan <patil.jivan99@gmail.com>
2023-01-04 15:37:14 +0100

    two sum and duplicate elements

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,3 +1,6 @@
+import org.junit.jupiter.api.Test;
+import sorting.ArrayUtility;
+
 import java.util.*;
 
 /**
@@ -9,4 +12,25 @@ import java.util.*;
 public class TestClass {
 
 
+    /*
+    * 121. Best Time to Buy and Sell Stock
+    * */
+    public int maxProfit(int[] prices) {
+        int currentMin = Integer.MAX_VALUE;
+        int profit = 0;
+
+        for(int i : prices){
+            if( i < currentMin){
+                currentMin = Math.min( i , currentMin);
+            }
+
+            profit = Math.max( profit , i - currentMin);
+        }
+        return profit;
+    }
+
+    @Test
+    public void test(){
+        System.out.println(maxProfit(new int[]{7,6,4,3,1}));
+    }
 }
