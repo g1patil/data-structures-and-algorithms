35a65c8fc2529badb5c3fc4e7219ccb6aef77f25
Jivan <patil.jivan99@gmail.com>
2023-01-05 13:27:40 +0100

    //1480. Running Sum of 1d Array

--- src/TestClass.java
+++ src/TestClass.java
@@ -13,45 +13,18 @@ import java.util.*;
 public class TestClass {
 
 
-    /*
-    * 98. Validate Binary Search Tree
-    * */
-    public boolean isValidBST(TreeNode root) {
-        Stack<TreeNode> stack = new Stack<>();
-        TreeNode temp = root;
-        TreeNode previous = null;
-        while(temp !=null || !stack.isEmpty()){
-            while(temp!=null){
-                stack.push(temp);
-                temp = temp.left;
-            }
-            temp = stack.pop();
-            if(previous!=null && temp.val <= previous.val){
-                return false;
-            }
-            previous = temp;
-            temp = temp.right;
+
+    //1480. Running Sum of 1d Array
+    public int[] runningSum(int[] nums) {
+        for(int i = 1; i < nums.length; i++) {
+            nums[i]+=nums[i];
         }
-        return true;
+        return nums;
     }
-
-
 
 
     @Test
     public void test(){
-        TreeNode n1 = new TreeNode(1);
-        TreeNode n2 = new TreeNode(2);
-        TreeNode n3 = new TreeNode(3);
-        TreeNode n4 = new TreeNode(4);
-        TreeNode n5 = new TreeNode(5);
-        TreeNode n6 = new TreeNode(6);
-        TreeNode n7 = new TreeNode(7);
 
-        n2.setChild(n1,n3);
-        n4.setChild(n2,n6);
-        n6.setChild(n5,n7);
-
-        System.out.println(isValidBST(n4));
     }
 }
