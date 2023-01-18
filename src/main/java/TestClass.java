30cec6c8b2ff6a66759913323c9872e4e076ce18
Jivan <patil.jivan99@gmail.com>
2023-01-18 14:23:33 +0100

    //Moving Average from Data Stream - practice

--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -1,3 +1,5 @@
+import java.util.LinkedList;
+import java.util.Queue;
 
 /**
  * Test class to practice any given problem .
@@ -7,6 +9,24 @@
 
 public class TestClass {
 
+    private int size;
+    int sum = 0;
+    Queue<Integer> q = new LinkedList();
+    //Moving Average from Data Stream
 
+    public MovingAverage(int size) {
+        this.size = size;
+    }
 
+    public double next(int val) {
+        sum+=val;
+        q.add(val);
+
+        if(q.size() <= size){
+            return sum/q.size();
+        } else {
+            sum-=q.remove();
+        }
+        return sum/size;
+    }
 }
