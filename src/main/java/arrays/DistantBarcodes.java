adbf69238faf6c2f7e593f1b60e0bac5f805ea9f
jivanpatil <patil.jivan99@gmail.com>
2023-01-19 22:27:50 +0100

    1054. Distant Barcodes - wip

---
+++ src/main/java/arrays/DistantBarcodes.java
@@ -0,0 +1,18 @@
+package arrays;
+
+import java.util.Arrays;
+
+/**
+ * 1054. Distant Barcodes
+ * */
+public class DistantBarcodes {
+
+    public int[] rearrangeBarcodes(int[] barcodes) {
+        int[] result = new int[barcodes.length];
+        Arrays.sort(barcodes);
+
+        int p1 = 0 , p2 = barcodes.length-1;
+
+
+    }
+}
