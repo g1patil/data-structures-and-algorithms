2bb7d20cd9cd63989d0eef4765803ee425c9b709
Jivan <patil.jivan99@gmail.com>
2023-01-25 20:04:23 +0100

    make the code pretty again

--- src/main/java/lists/EmployeePair.java
+++ src/main/java/lists/EmployeePair.java
@@ -7,23 +7,23 @@ import java.util.*;
 /**
  * Given list of emplyees, pair each emplyee.
  * If the method is called twice, they should not be paired again.
- * */
+ */
 public class EmployeePair<T> {
 
-    private final Map<Object,Set<Object>> pairsMap = new HashMap<>();
+    private final Map<Object, Set<Object>> pairsMap = new HashMap<>();
 
     public List<List<T>> makePair(List<T> employeeList){
         Collections.shuffle(employeeList);
         List<List<T>> result = new ArrayList<>();
 
-        for(int i = 0; i < employeeList.size(); i+=2) {
+        for(int i = 0; i < employeeList.size(); i += 2) {
             T a = employeeList.get(i);
             T b = employeeList.get(i+1);
-            if(!alreadyPaired(a,b)){
-                addToPairHistory(a,b);
-                result.add( List.of(a,b) );
-            } else {
-                if(pairsMap.get(employeeList.get(0)).size() == employeeList.size() - 1)
+            if(!alreadyPaired(a, b)){
+                addToPairHistory(a, b);
+                result.add(List.of(a, b));
+            } else{
+                if(pairsMap.get(employeeList.get(0)).size() == employeeList.size()-1)
                     return result;
                 return makePair(employeeList);
             }
@@ -32,16 +32,15 @@ public class EmployeePair<T> {
         return result;
     }
 
-    private void addToPairHistory(T a , T b){
-        pairsMap.putIfAbsent(a,new HashSet<>());
-        pairsMap.putIfAbsent(b,new HashSet<>());
+    private void addToPairHistory(T a, T b){
+        pairsMap.putIfAbsent(a, new HashSet<>());
+        pairsMap.putIfAbsent(b, new HashSet<>());
         pairsMap.get(a).add(b);
         pairsMap.get(b).add(a);
     }
 
-
-    private boolean alreadyPaired(T a , T b){
-        return pairsMap.getOrDefault(a , Set.of()).contains(b)
+    private boolean alreadyPaired(T a, T b){
+        return pairsMap.getOrDefault(a, Set.of()).contains(b)
                 || pairsMap.getOrDefault(b, Set.of()).contains(a);
     }
 
