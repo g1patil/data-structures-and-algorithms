30c9ae65bd90577083922dc4cf2439db7654e9ca
jivanpatil <patil.jivan99@gmail.com>
2023-02-09 12:42:19 +0100

    707. Design Linked List - final

--- src/main/java/ood/design/MyLinkedList.java
+++ src/main/java/ood/design/MyLinkedList.java
@@ -8,11 +8,12 @@ import annotation.Stage;
 /**
  * 707. Design Linked List
  * */
-@Quality(value = Stage.FAILING,details = "8/64")
+@Quality(value = Stage.TESTED)
 @Platform(Site.LEETCODE)
 public class MyLinkedList {
 
     private Node head;
+    private int size;
 
     private static class Node {
         public int value;
@@ -27,19 +28,22 @@ public class MyLinkedList {
     }
 
     public int get(int index) {
+        if (index<0)
+            return -1;
         Node temp = head;
         int p1=0;
         while(p1 != index){
             p1++;
             temp = temp.next;
         }
-        return temp.next.value;
+        return temp == null || temp.next == null ? -1 : temp.next.value;
     }
 
     public void addAtHead(int val) {
         Node node = new Node(val);
         node.next = head.next;
         head.next = node;
+        size++;
     }
 
     public void addAtTail(int val) {
@@ -49,21 +53,35 @@ public class MyLinkedList {
             temp = temp.next;
         }
         temp.next = node;
+        size++;
     }
 
     public void addAtIndex(int index, int val) {
-        int p1=0;
+        if (index > size || index < 0)
+            return;
         Node temp = head;
-        while(p1!=index){
-            p1++;
-            temp = temp.next;
-        }
         Node node = new Node(val);
-        node.next = temp.next;
-        temp.next = node;
+        if (size == index){
+            while (temp.next != null)
+                temp = temp.next;
+            temp.next = node;
+            size++;
+        } else {
+            int p1=0;
+            while(p1!=index){
+                p1++;
+                temp = temp.next;
+            }
+
+            node.next = temp.next;
+            temp.next = node;
+            size++;
+        }
     }
 
     public void deleteAtIndex(int index) {
+        if (index>=size)
+            return;
         int p1=0;
         Node temp = head;
         while(p1!=index){
@@ -75,17 +93,19 @@ public class MyLinkedList {
     }
 
     public static void main(String[] args) {
+
         MyLinkedList obj = new MyLinkedList();
-        obj.addAtHead(2);
+        obj.addAtHead(1);
+        obj.addAtTail(3);
+        obj.addAtIndex(1, 2);
+        obj.get(1);
         obj.deleteAtIndex(1);
-        obj.addAtHead(2);
-        obj.addAtHead(7);
-        obj.addAtHead(3);
-        obj.addAtHead(2);
-        obj.addAtHead(5);
-        obj.addAtTail(5);
-        obj.get(5);
-        obj.deleteAtIndex(6);
-        obj.deleteAtIndex(4);
+        obj.get(1);
+        obj.get(3);
+        obj.deleteAtIndex(3);
+        obj.deleteAtIndex(0);
+        obj.get(0);
+        obj.deleteAtIndex(0);
+        obj.get(0);
     }
 }
