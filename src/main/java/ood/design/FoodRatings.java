c7a306c6490b1e297c9e231a5d80db8a2030ee5e
Jivan <patil.jivan99@gmail.com>
2023-02-09 20:52:52 +0100

    2353. Design a Food Rating System - final

---
+++ src/main/java/ood/design/FoodRatings.java
@@ -0,0 +1,51 @@
+package ood.design;
+
+import java.util.*;
+
+public class FoodRatings {
+
+    private Map<String, TreeMap<Integer, TreeSet<String>>> map ;
+    private Map<String, String> foodCousineMap ;
+    private Map<String, Integer> foodRatingMap ;
+
+    public FoodRatings(String[] foods, String[] cuisines, int[] ratings) {
+        map = new HashMap<>();
+        int length = foods.length;
+        foodCousineMap = new HashMap<>();
+        foodRatingMap = new HashMap<>();
+
+        for(int i = 0; i < length; i++) {
+            foodCousineMap.put(foods[i],cuisines[i]);
+            foodRatingMap.put(foods[i],  ratings[i]);
+
+            map.putIfAbsent(cuisines[i] , new TreeMap<>(Comparator.reverseOrder()));
+            map.get(cuisines[i]).putIfAbsent( ratings[i],new TreeSet<>());
+            map.get(cuisines[i]).get( ratings[i]).add(foods[i]);
+        }
+    }
+
+    public void changeRating(String food, int newRating) {
+        int oldRating = foodRatingMap.get(food);
+        String cousine = foodCousineMap.get(food);
+
+        map.get(cousine).get(oldRating).remove(food);
+        if(map.get(cousine).get(oldRating).isEmpty())
+            map.get(cousine).remove(oldRating);
+        map.get(cousine).putIfAbsent(newRating,new TreeSet<>());
+        map.get(cousine).get(newRating).add(food);
+        foodRatingMap.put(food,newRating);
+    }
+
+    public String highestRated(String cuisine) {
+        int highestRating = map.get(cuisine).keySet().stream().findFirst().get();
+        return map.get(cuisine).get(highestRating).first();
+    }
+
+    public static void main(String[] args){
+        FoodRatings obj = new FoodRatings(new String[] {"biihw"}, new String[] {"okxsrcqn"}, new int[] {13});
+        obj.changeRating("biihw", 9);
+        obj.changeRating("biihw", 6);
+
+
+    }
+}
