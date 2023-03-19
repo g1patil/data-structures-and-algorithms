00833f8303afdc66d3ad2677244a9cc733dad6ef
Jivan <patil.jivan99@gmail.com>
2023-03-19 23:47:39 +0100

    941. Valid Mountain Array - wip

---
+++ src/main/java/arrays/ValidMountainArray.java
@@ -0,0 +1,15 @@
+package arrays;
+
+import annotation.Quality;
+import annotation.Stage;
+
+/*
+* 941. Valid Mountain Array
+* */
+@Quality(Stage.INCOMPLETE)
+public class ValidMountainArray {
+
+    public boolean validMountainArray(int[] arr) {
+        return false;
+    }
+}
